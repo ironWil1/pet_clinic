1ee3713f2b6aa17ba5a3a7e08949dfa892bda964
Artem Kulin <kulinartem@gmail.com>
2021-04-26 23:41:49 +0300

    Add ReadOnlyDaoImpl class

--- dao-abstract/src/main/java/com/vet24/dao/ReadOnlyDaoImpl.java
+++ dao-abstract/src/main/java/com/vet24/dao/ReadOnlyDaoImpl.java
@@ -1,2 +1,40 @@
-package com.vet24.dao;public class ReadOnlyDaoImpl {
+package com.vet24.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+import java.util.List;
+
+public class ReadOnlyDaoImpl<K extends Serializable, T> implements ReadOnlyDao<K, T> {
+
+    @PersistenceContext
+    EntityManager manager;
+
+    private Class<T> type;
+
+    public Class<T> getType() {
+        return type;
+    }
+
+    public void setType(Class<T> type) {
+        this.type = type;
+    }
+
+    @Override
+    public T getByKey(K key) {
+        return manager.find(type, key);
+    }
+
+    @Override
+    public boolean isExistByKey(K key) {
+        return manager.find(type, key) == null;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<T> getAll() {
+        return manager
+                .createQuery("FROM " + type.getName())
+                .getResultList();
+    }
 }
