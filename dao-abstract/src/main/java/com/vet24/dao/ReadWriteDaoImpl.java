6c8aa6d5569eced3f1d9c5e005a2f8901a301b5f
Artem Kulin <kulinartem@gmail.com>
2021-04-27 00:19:09 +0300

    Add ReadWriteDaoImpl class

---
+++ dao-abstract/src/main/java/com/vet24/dao/ReadWriteDaoImpl.java
@@ -0,0 +1,44 @@
+package com.vet24.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReadWriteDaoImpl<K extends Serializable, T> extends ReadOnlyDaoImpl<K, T> implements ReadWriteDao<K, T> {
+
+    @PersistenceContext
+    EntityManager manager;
+
+    @Override
+    public void persist(T entity) {
+        manager.persist(entity);
+    }
+
+    @Override
+    public void persistAll(Collection<T> entities) {
+        entities.forEach(elem -> manager.persist(elem));
+    }
+
+    @Override
+    public T update(T entity) {
+        return manager.merge(entity);
+    }
+
+    @Override
+    public List<T> updateAll (List<T> entities) {
+        return entities.stream().map(elem -> manager.merge(elem)).collect(Collectors.toList());
+    }
+
+    @Override
+    public void delete(T entity) {
+        manager.remove(entity);
+    }
+
+    @Override
+    public void deleteAll(Collection<T> entities) {
+        entities.forEach(elem -> manager.remove(elem));
+    }
+}
