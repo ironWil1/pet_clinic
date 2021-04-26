53fff11b76edb862bff00ebabfb71353d1ecfd39
dvvlet <daulet.zholdasbek@gmai.com>
2021-04-26 19:24:36 +0600

    task 1 done

--- web/src/main/java/com/vet24/web/PetClinicApplication.java
+++ web/src/main/java/com/vet24/web/PetClinicApplication.java
@@ -1,7 +1,9 @@
-package com.vet24.pet_clinic;
+package com.vet24.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class PetClinicApplication {
