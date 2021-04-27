db6af49f1687eb2a7f9d86a7b852d15327b329a9
Polina Kolzunova <polinakolzunova@gmail.com>
2021-04-27 21:08:20 +0300

    Add: ResourceServiceImpl & UploadServiceImpl empty class

---
+++ service-impl/src/main/java/com/vet24/service/ResourceServiceImpl.java
@@ -0,0 +1,13 @@
+package com.vet24.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.core.io.Resource;
+
+@Service
+public class ResourceServiceImpl implements ResourceService {
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        return null;
+    }
+}
---
+++ service-impl/src/main/java/com/vet24/service/UploadServiceImpl.java
@@ -0,0 +1,13 @@
+package com.vet24.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadServiceImpl implements UploadService {
+
+    @Override
+    public String store(MultipartFile file) {
+        return null;
+    }
+}
