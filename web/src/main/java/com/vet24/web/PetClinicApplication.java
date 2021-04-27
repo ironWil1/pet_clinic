902100b149bc2991b016e3c9a3dcc19408f49983
Polina Kolzunova <polinakolzunova@gmail.com>
2021-04-27 21:05:24 +0300

    Modify PetClinicApplication: add scan base packages parameter

--- web/src/main/java/com/vet24/web/PetClinicApplication.java
+++ web/src/main/java/com/vet24/web/PetClinicApplication.java
@@ -2,10 +2,8 @@ package com.vet24.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.vet24")
 public class PetClinicApplication {
 
     public static void main(String[] args) {
