49e8f178fc08f8976d8c54312ddc7b11cec08ced
Polina Kolzunova <polinakolzunova@gmail.com>
2021-04-28 22:05:21 +0300

    update add upload-folder property & content type for ResourceService

--- service-impl/src/main/java/com/vet24/service/media/ResourceServiceImpl.java
+++ service-impl/src/main/java/com/vet24/service/media/ResourceServiceImpl.java
@@ -1,43 +1,50 @@
 package com.vet24.service.media;
 
 import com.vet24.models.exception.StorageException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StreamUtils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@PropertySource("application.properties")
 public class ResourceServiceImpl implements ResourceService {
 
-    private final String contentTypeDefault = "application/octet-stream";
+    @Value("${vet24.service.media.upload-folder:uploads/}")
+    private String uploadFolder;
 
-    private final Map<String, String> contentTypeMap = new HashMap<>(){{
-        put(".mp4","video/mp4");
-        put(".mpeg","video/mpeg");
-        put(".mpg","video/mpeg");
-        put(".ogg","video/ogg");
-        put(".3gpp","video/3gpp");
-        put(".wmv","video/x-ms-wmv");
-        put(".flv","video/x-flv");
-        put(".jpeg","image/jpeg");
-        put(".jpe","image/jpeg");
-        put(".jpg","image/jpeg");
-        put(".gif","image/gif");
-        put(".png","image/png");
-        put(".tiff","image/tiff");
-        put(".tif","image/tiff");
-        put(".mid","audio/midi");
-        put(".midi","audio/midi");
-        put(".mp3","audio/mpeg");
-        put(".wav","audio/vnd.wav");
-        put(".flac","audio/flac");
-        put(".m4a","audio/mp4");
-        put(".m4b","audio/mp4");
-        put(".m3u","audio/x-mpegurl");
-        put(".au","audio/basic");
+    private String contentTypeDefault = "application/octet-stream";
+
+    private Map<String, String> contentTypeMap = new HashMap<>() {{
+        put(".mp4", "video/mp4");
+        put(".mpeg", "video/mpeg");
+        put(".mpg", "video/mpeg");
+        put(".ogg", "video/ogg");
+        put(".3gpp", "video/3gpp");
+        put(".wmv", "video/x-ms-wmv");
+        put(".flv", "video/x-flv");
+        put(".jpeg", "image/jpeg");
+        put(".jpe", "image/jpeg");
+        put(".jpg", "image/jpeg");
+        put(".gif", "image/gif");
+        put(".png", "image/png");
+        put(".tiff", "image/tiff");
+        put(".tif", "image/tiff");
+        put(".mid", "audio/midi");
+        put(".midi", "audio/midi");
+        put(".mp3", "audio/mpeg");
+        put(".wav", "audio/vnd.wav");
+        put(".flac", "audio/flac");
+        put(".m4a", "audio/mp4");
+        put(".m4b", "audio/mp4");
+        put(".m3u", "audio/x-mpegurl");
+        put(".au", "audio/basic");
     }};
 
     @Override
@@ -45,7 +52,7 @@ public class ResourceServiceImpl implements ResourceService {
         String extension = filename.substring(filename.lastIndexOf("."));
         String contentType = contentTypeDefault;
 
-        if(contentTypeMap.containsKey(extension)) {
+        if (contentTypeMap.containsKey(extension)) {
             contentType = contentTypeMap.get(extension);
         }
 
@@ -54,10 +61,8 @@ public class ResourceServiceImpl implements ResourceService {
 
     @Override
     public byte[] loadAsByteArray(String filename) {
-        filename = "uploads/" + filename;
-
-        try (FileInputStream fis = new FileInputStream(filename)) {
-            return StreamUtils.copyToByteArray(fis);
+        try (InputStream is = new FileSystemResource(uploadFolder + filename).getInputStream()) {
+            return StreamUtils.copyToByteArray(is);
         } catch (IOException e) {
             throw new StorageException("Could not read file: " + filename);
         }
--- service-impl/src/main/java/com/vet24/service/media/UploadServiceImpl.java
+++ service-impl/src/main/java/com/vet24/service/media/UploadServiceImpl.java
@@ -1,7 +1,8 @@
 package com.vet24.service.media;
 
 import com.vet24.models.exception.StorageException;
-import com.vet24.service.media.UploadService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,7 +17,11 @@ import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 @Service
+@PropertySource("application.properties")
 public class UploadServiceImpl implements UploadService {
+
+    @Value("${vet24.service.media.upload-folder:uploads/}")
+    private String uploadFolder;
 
     @Override
     public String store(MultipartFile file) {
@@ -28,22 +33,22 @@ public class UploadServiceImpl implements UploadService {
 
         LocalDateTime now = LocalDateTime.now();
         int extensionIndex = originFilename.lastIndexOf(".");
-        String storagePath = "uploads/" + now.format(DateTimeFormatter.ofPattern("yyyy/MM/dd/"));
+        String storageFolder = uploadFolder + now.format(DateTimeFormatter.ofPattern("yyyy/MM/dd/"));
         String storageFilename = originFilename.substring(0, extensionIndex)
-                + now.format(DateTimeFormatter.ofPattern("-yyyyMMddHHmmss"))
+                + now.format(DateTimeFormatter.ofPattern("_yyyy-MM-dd_HH-mm-ss"))
                 + originFilename.substring(extensionIndex);
 
         try (InputStream inputStream = file.getInputStream()) {
 
-            if (!Files.exists(Paths.get(storagePath))) {
-                Files.createDirectories(Paths.get(storagePath));
+            if (!Files.exists(Paths.get(storageFolder))) {
+                Files.createDirectories(Paths.get(storageFolder));
             }
-            Files.copy(inputStream, Paths.get(storagePath + storageFilename),
+            Files.copy(inputStream, Paths.get(storageFolder + storageFilename),
                     StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             throw new StorageException("Failed to store file " + originFilename);
         }
 
-        return storagePath + storageFilename;
+        return storageFolder + storageFilename;
     }
 }
