a4e673438ed5190a1419bcff06f3e3a565a94653
Artem Kulin <kulinartem@gmail.com>
2021-04-28 13:33:10 +0300

    Add InMemorySecurityConfig class

---
+++ security/src/main/java/com/vet24/security/InMemorySecurityConfig.java
@@ -0,0 +1,35 @@
+package com.vet24.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class InMemorySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    protected UserDetailsService users () {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("admin")
+                .roles("admin")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+}
