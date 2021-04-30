06b15659a4df28ff2790ad826e200283c4f375ea
Polina Kolzunova <polinakolzunova@gmail.com>
2021-04-30 10:19:29 +0300

    swagger update uploads/upload-file path

--- web/src/main/java/com/vet24/web/config/OpenApiConfig.java
+++ web/src/main/java/com/vet24/web/config/OpenApiConfig.java
@@ -1,8 +1,14 @@
 package com.vet24.web.config;
 
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +19,30 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption,
                                  @Value("${application-version}") String appVersion) {
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .info(new Info()
                         .title("Pet Clinic API")
                         .version(appVersion)
                         .description(appDesciption)
                         .termsOfService("http://swagger.io/terms/")
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+        openAPI.path("/api/uploads/upload-file", new PathItem()
+                .post(new Operation()
+                        .requestBody(new RequestBody()
+                                .content(new Content()
+                                        .addMediaType("multipart/form-data", new MediaType()
+                                                .schema(new Schema<>()
+                                                        .addRequiredItem("file")
+                                                        .type("object")
+                                                        .addProperties("file", new Schema<>().type("string").format("binary"))
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
+
+        return openAPI;
     }
 }
--- web/src/main/java/com/vet24/web/controllers/media/UploadController.java
+++ web/src/main/java/com/vet24/web/controllers/media/UploadController.java
@@ -2,6 +2,9 @@ package com.vet24.web.controllers.media;
 
 import com.vet24.service.media.UploadService;
 import com.vet24.web.dto.FileDto;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +23,7 @@ public class UploadController {
     }
 
     @PostMapping("/upload-file")
+    @ApiResponse(responseCode = "200", description = "Successful upload", content = @Content(schema = @Schema(implementation = FileDto.class)))
     public ResponseEntity<FileDto> uploadFile(@RequestParam("file") MultipartFile file){
         String storageUrl = uploadService.store(file);
         FileDto uploadFile = new FileDto(file.getOriginalFilename(), storageUrl);
