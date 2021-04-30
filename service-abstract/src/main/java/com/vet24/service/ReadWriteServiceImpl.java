fc1345952be64d8b1b2fae80040cf9b7c628cb11
Artem Kulin <kulinartem@gmail.com>
2021-05-01 00:05:05 +0300

    Modify ReadWriteServiceImpl: change class to abstract, add annotations to all methods

--- service-abstract/src/main/java/com/vet24/service/ReadWriteServiceImpl.java
+++ service-abstract/src/main/java/com/vet24/service/ReadWriteServiceImpl.java
@@ -2,48 +2,51 @@ package com.vet24.service;
 
 import com.vet24.dao.ReadOnlyDao;
 import com.vet24.dao.ReadWriteDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 import java.util.List;
 
-@Service
-public class ReadWriteServiceImpl<K extends Serializable, T> extends ReadOnlyServiceImpl<K, T> implements ReadWriteService<K, T> {
+public abstract class ReadWriteServiceImpl<K extends Serializable, T> extends ReadOnlyServiceImpl<K, T> implements ReadWriteService<K, T> {
 
     private final ReadWriteDao<K, T> readWriteDao;
 
-    @Autowired
     public ReadWriteServiceImpl(ReadOnlyDao<K, T> readOnlyDao, ReadWriteDao<K, T> readWriteDao) {
         super(readOnlyDao);
         this.readWriteDao = readWriteDao;
     }
 
+    @Transactional
     @Override
     public void persist(T entity) {
         readWriteDao.persist(entity);
     }
 
+    @Transactional
     @Override
     public void persistAll(List<T> entities) {
         readWriteDao.persistAll(entities);
     }
 
+    @Transactional
     @Override
     public T update(T entity) {
         return readWriteDao.update(entity);
     }
 
+    @Transactional
     @Override
     public List<T> updateAll(List<T> entities) {
         return readWriteDao.updateAll(entities);
     }
 
+    @Transactional
     @Override
     public void delete(T entity) {
         readWriteDao.delete(entity);
     }
 
+    @Transactional
     @Override
     public void deleteAll(List<T> entities) {
         readWriteDao.deleteAll(entities);
