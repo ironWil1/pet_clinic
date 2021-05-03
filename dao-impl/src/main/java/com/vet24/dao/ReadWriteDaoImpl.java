bec5f51ad54ac430b8e002607b1fca49eb7ffe58
Artem Kulin <kulinartem@gmail.com>
2021-05-03 20:44:35 +0300

    Modify ReadWriteDiImpl: delete implementation & rework persistAll method

--- dao-impl/src/main/java/com/vet24/dao/ReadWriteDaoImpl.java
+++ dao-impl/src/main/java/com/vet24/dao/ReadWriteDaoImpl.java
@@ -1,39 +1,40 @@
 package com.vet24.dao;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.io.Serializable;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public abstract class ReadWriteDaoImpl<K extends Serializable, T> extends ReadOnlyDaoImpl<K, T> implements ReadWriteDao<K, T> {
+public abstract class ReadWriteDaoImpl<K extends Serializable, T> extends ReadOnlyDaoImpl<K, T> {
 
-    @Override
     public void persist(T entity) {
         manager.persist(entity);
     }
 
-    @Override
     public void persistAll(List<T> entities) {
         entities.forEach(elem -> manager.persist(elem));
+        int count = 0;
+        while (entities.listIterator().hasNext()) {
+            ++count;
+            if (count % 100 == 0) {
+                manager.flush();
+                manager.clear();
+            }
+            entities.listIterator().next();
+        }
     }
 
-    @Override
     public T update(T entity) {
         return manager.merge(entity);
     }
 
-    @Override
     public List<T> updateAll (List<T> entities) {
         return entities.stream().map(elem -> manager.merge(elem)).collect(Collectors.toList());
     }
 
-    @Override
     public void delete(T entity) {
         manager.remove(entity);
     }
 
-    @Override
     public void deleteAll(List<T> entities) {
         entities.forEach(elem -> manager.remove(elem));
     }
