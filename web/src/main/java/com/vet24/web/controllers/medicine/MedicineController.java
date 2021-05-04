3dfe61bc73bd5e3626ad3599b5d3c92ecd91ce60
dvvlet <daulet.zholdasbek@gmai.com>
2021-05-05 01:12:56 +0600

    adding upload and get icon methods

--- web/src/main/java/com/vet24/web/controllers/medicine/MedicineController.java
+++ web/src/main/java/com/vet24/web/controllers/medicine/MedicineController.java
@@ -27,10 +27,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-
 
 @RestController
 @RequestMapping("api/manager/medicine")
