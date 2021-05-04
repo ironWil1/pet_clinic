f7e09b7f387e287f7505554310cde254065adfe1
Polina Kolzunova <polinakolzunova@gmail.com>
2021-05-04 14:48:37 +0300

    fix: make field static

--- service-impl/src/main/java/com/vet24/service/media/ResourceServiceImpl.java
+++ service-impl/src/main/java/com/vet24/service/media/ResourceServiceImpl.java
@@ -10,9 +10,8 @@ import org.springframework.util.StreamUtils;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @PropertySource("application.properties")
@@ -23,33 +22,34 @@ public class ResourceServiceImpl implements ResourceService {
 
     private String contentTypeDefault = "application/octet-stream";
 
-    private Map<String, String> contentTypeMap = Stream.of(new String[][]{
-            {".mp4", "video/mp4"},
-            {".mpeg", "video/mpeg"},
-            {".mpg", "video/mpeg"},
-            {".ogg", "video/ogg"},
-            {".3gpp", "video/3gpp"},
-            {".wmv", "video/x-ms-wmv"},
-            {".flv", "video/x-flv"},
-            {".jpeg", "image/jpeg"},
-            {".jpe", "image/jpeg"},
-            {".jpg", "image/jpeg"},
-            {".gif", "image/gif"},
-            {".png", "image/png"},
-            {".tiff", "image/tiff"},
-            {".tif", "image/tiff"},
-            {".mid", "audio/midi"},
-            {".midi", "audio/midi"},
-            {".mp3", "audio/mpeg"},
-            {".wav", "audio/vnd.wav"},
-            {".flac", "audio/flac"},
-            {".m4a", "audio/mp4"},
-            {".m4b", "audio/mp4"},
-            {".m3u", "audio/x-mpegurl"},
-            {".au", "audio/basic"}
-    }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
+    private static Map<String, String> contentTypeMap = new HashMap<>();
 
-    @Override
+    static {
+        contentTypeMap.put(".mp4", "video/mp4");
+        contentTypeMap.put(".mpeg", "video/mpeg");
+        contentTypeMap.put(".mpg", "video/mpeg");
+        contentTypeMap.put(".ogg", "video/ogg");
+        contentTypeMap.put(".3gpp", "video/3gpp");
+        contentTypeMap.put(".wmv", "video/x-ms-wmv");
+        contentTypeMap.put(".flv", "video/x-flv");
+        contentTypeMap.put(".jpeg", "image/jpeg");
+        contentTypeMap.put(".jpe", "image/jpeg");
+        contentTypeMap.put(".jpg", "image/jpeg");
+        contentTypeMap.put(".gif", "image/gif");
+        contentTypeMap.put(".png", "image/png");
+        contentTypeMap.put(".tiff", "image/tiff");
+        contentTypeMap.put(".tif", "image/tiff");
+        contentTypeMap.put(".mid", "audio/midi");
+        contentTypeMap.put(".midi", "audio/midi");
+        contentTypeMap.put(".mp3", "audio/mpeg");
+        contentTypeMap.put(".wav", "audio/vnd.wav");
+        contentTypeMap.put(".flac", "audio/flac");
+        contentTypeMap.put(".m4a", "audio/mp4");
+        contentTypeMap.put(".m4b", "audio/mp4");
+        contentTypeMap.put(".m3u", "audio/x-mpegurl");
+        contentTypeMap.put(".au", "audio/basic");
+    }
+
     public String getContentTypeByFileName(String filename) {
         int extensionIndex = filename.lastIndexOf(".");
 
