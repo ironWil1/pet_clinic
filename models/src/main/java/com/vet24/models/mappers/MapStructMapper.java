2ba491001b445cb8a4c71895efaf7cf38013759f
Cloud <ruwsps@gmail.com>
2021-05-04 12:40:20 +0300

    Can add new Dog in POST endpoint now. Updated MapStructMapper

--- models/src/main/java/com/vet24/models/mappers/MapStructMapper.java
+++ models/src/main/java/com/vet24/models/mappers/MapStructMapper.java
@@ -1,7 +1,10 @@
 package com.vet24.models.mappers;
 
+import com.vet24.models.dtos.CatDto;
 import com.vet24.models.dtos.ClientDto;
+import com.vet24.models.dtos.DogDto;
 import com.vet24.models.dtos.PetDto;
+import com.vet24.models.pet.Dog;
 import com.vet24.models.pet.Pet;
 import com.vet24.models.user.Client;
 import org.mapstruct.Mapper;
@@ -16,6 +19,16 @@ public interface MapStructMapper {
     @Mapping(source = "login", target = "username")
     ClientDto clientToClientDto(Client client);
 
-    @Mapping(source = "petType", target = "type")
-    PetDto petToPetDto(Pet pet);
+//    @Mapping(source = "petType", target = "type")
+//    PetDto petToPetDto(Pet pet);
+
+// abstract
+//    @Mapping(source = "type", target = "petType")
+//    Pet PetDtoToPet(PetDto petDto);
+
+    Dog DogDtoToDog(DogDto dogDto);
+
+    DogDto DogToDogDto(Dog dog);
+
+//    Cat CatDtoToCat(CatDto catDto);
 }
--- web/src/main/java/com/vet24/web/controllers/ClientController.java
+++ web/src/main/java/com/vet24/web/controllers/ClientController.java
@@ -1,23 +1,25 @@
 package com.vet24.web.controllers;
 
-import com.vet24.models.dtos.ClientDto;
+import com.vet24.models.dtos.*;
 import com.vet24.models.mappers.MapStructMapper;
+import com.vet24.models.pet.Dog;
+import com.vet24.models.pet.Pet;
+import com.vet24.service.pet.PetService;
 import com.vet24.service.user.ClientService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/client")
 public class ClientController {
 
     private final ClientService clientService;
+    private final PetService petService;
     private final MapStructMapper mapStructMapper;
 
-    public ClientController(ClientService clientService, MapStructMapper mapStructMapper) {
+    public ClientController(ClientService clientService, PetService petService, MapStructMapper mapStructMapper) {
         this.clientService = clientService;
+        this.petService = petService;
         this.mapStructMapper = mapStructMapper;
     }
 
@@ -28,6 +30,15 @@ public class ClientController {
     }
 
     // POST /api/client/pet/add
+    @PostMapping("/{id}/pet/add")
+    public ResponseEntity<?> addPet(@PathVariable("id") Long id, @RequestBody AbstractNewPetDto petDto) {
+        if (petDto instanceof DogDto) {
+            System.out.println("petDto instanceof DogDto");
+            petService.save(mapStructMapper.DogDtoToDog((DogDto) petDto));
+            return ResponseEntity.ok(petDto);
+        }
+        return ResponseEntity.badRequest().build();
+    }
 
     // DELETE /api/client/pet/{petId} само собой удалить можно только своего питомца
 
