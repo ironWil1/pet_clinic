2460c13a2604b9fe2c8f5a08ea90230957743199
Artem Kulin <kulinartem@gmail.com>
2021-05-05 21:38:35 +0300

    Modify RoleService: add ReadWriteService interface implementation & delete same methods

--- service-abstract/src/main/java/com/vet24/service/user/RoleService.java
+++ service-abstract/src/main/java/com/vet24/service/user/RoleService.java
@@ -1,13 +1,9 @@
 package com.vet24.service.user;
 
 import com.vet24.models.user.Role;
+import com.vet24.service.ReadWriteService;
 
 import java.util.List;
 
-public interface RoleService {
-    Role getRoleById(Long id);
-    List<Role> getAllRoles();
-    void addRole(Role role);
-    void editRole(Role role);
-    void deleteRole(Long id);
+public interface RoleService extends ReadWriteService<Long, Role> {
 }
