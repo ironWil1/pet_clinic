aec45c4dd9df1c8b551139772c0faa945cc29bc8
Artem Kulin <kulinartem@gmail.com>
2021-05-05 12:52:04 +0300

    Rework UserDaoImpl: add extends from ReadWriteDaoImpl % delete same methods

--- dao-impl/src/main/java/com/vet24/dao/user/UserDaoImpl.java
+++ dao-impl/src/main/java/com/vet24/dao/user/UserDaoImpl.java
@@ -1,54 +1,22 @@
 package com.vet24.dao.user;
 
+import com.vet24.dao.ReadWriteDaoImpl;
 import com.vet24.models.user.User;
-
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 
 @Repository
-public class UserDaoImpl implements UserDao {
+public class UserDaoImpl extends ReadWriteDaoImpl<Long, User> implements UserDao {
 
     @PersistenceContext
-    EntityManager entityManager;
-
-    @Override
-    public User getUserById(Long id) {
-        User user = entityManager.find(User.class, id);
-        return user;
-    }
+    private EntityManager entityManager;
 
     @Override
     public User getUserByLogin(String login) {
-        User user = entityManager
+        return entityManager
                 .createQuery("from User where login =:login", User.class)
                 .setParameter("login", login).getSingleResult();
-        return user;
-    }
-
-    @Override
-    public List<User> getAllUsers() {
-        TypedQuery<User> query =
-                entityManager.createQuery("SELECT u FROM User u", User.class);
-        return query.getResultList();
-    }
-
-    @Override
-    public void addUser(User user) {
-        entityManager.persist(user);
-    }
-
-    @Override
-    public void editUser(User user) {
-        entityManager.merge(user);
-    }
-
-    @Override
-    public void deleteUser(Long id) {
-        entityManager.remove(getUserById(id));
     }
 }
