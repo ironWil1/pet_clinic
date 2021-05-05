c29177ddfe4b177ad337cfb1dd3989c6cd3ca9ba
dvvlet <daulet.zholdasbek@gmail.com>
2021-05-05 17:14:23 +0600

    delete unnecessary imports in mapper

--- models/src/main/java/com/vet24/models/mappers/MedicineMapper.java
+++ models/src/main/java/com/vet24/models/mappers/MedicineMapper.java
@@ -4,8 +4,7 @@ import com.vet24.models.dto.medicine.MedicineDto;
 import com.vet24.models.medicine.Medicine;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+
 
 @Mapper(componentModel = "spring")
 public interface MedicineMapper {
