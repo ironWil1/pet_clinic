4533abfbc9fb648566db5ac2d78011a20fad199b
Cloud <ruwsps@gmail.com>
2021-05-08 12:56:31 +0300

    Added Swagger annotations. Fixed NPE in GET avatar methods

--- web/src/main/java/com/vet24/web/controllers/ClientController.java
+++ web/src/main/java/com/vet24/web/controllers/ClientController.java
@@ -9,6 +9,12 @@ import com.vet24.service.media.ResourceService;
 import com.vet24.service.media.UploadService;
 import com.vet24.service.pet.PetService;
 import com.vet24.service.user.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -21,6 +27,7 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("api/client")
+@Tag(name = "client-controller", description = "operations with Clients and their Pets")
 public class ClientController {
 
     private final ClientService clientService;
@@ -38,13 +45,23 @@ public class ClientController {
         this.resourceService = resourceService;
     }
 
+    @Operation(summary = "get a Client by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the Client by id",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClientDto.class))),
+            @ApiResponse(responseCode = "404", description = "Client is not found", content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<ClientDto> getClientById(@PathVariable("id") Long id) {
         ClientDto clientDto = mapStructMapper.clientToClientDto(clientService.getByKey(id));
         return clientDto != null ? ResponseEntity.ok(clientDto) : ResponseEntity.notFound().build();
     }
 
-    // GET /api/client/avatar  получение изображения аватарки
+    @Operation(summary = "get avatar of a Client")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the avatar"),
+            @ApiResponse(responseCode = "404", description = "Client or avatar is not found")
+    })
     @GetMapping("/{id}/avatar")
     public ResponseEntity<byte[]> getClientAvatar(@PathVariable("id") Long id) {
         Client client = clientService.getByKey(id);
@@ -52,16 +69,25 @@ public class ClientController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
             String url = client.getAvatar();
-            String filename = StringUtils.substringAfterLast(url, File.separator);
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Content-Type", resourceService.getContentTypeByFileName(filename));
-            return url != null
+            HttpHeaders headers = null;
+            boolean isValidUrl;
+            if (isValidUrl = (url != null)) {
+                String filename = StringUtils.substringAfterLast(url, File.separator);
+                headers = new HttpHeaders();
+                headers.add("Content-Type", resourceService.getContentTypeByFileName(filename));
+            }
+            return isValidUrl
                     ? new ResponseEntity<>(resourceService.loadAsByteArray(url), headers, HttpStatus.OK)
                     : new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-    // POST /api/client/avatar сохранение новой аватарки
+    @Operation(summary = "upload avatar for a Client")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully uploaded the avatar",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = UploadedFileDto.class))),
+            @ApiResponse(responseCode = "404", description = "Client is not found", content = @Content)
+    })
     @PostMapping(value = "/{id}/avatar", consumes = {"multipart/form-data"})
     public ResponseEntity<UploadedFileDto> persistClientAvatar(@PathVariable("id") Long id,
                                                                @RequestParam("file") MultipartFile file) throws IOException {
@@ -76,7 +102,12 @@ public class ClientController {
         }
     }
 
-    // POST /api/client/pet/add
+    @Operation(summary = "add a new Pet", description = "Pet requires an existing client")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully added a new Pet",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = AbstractNewPetDto.class))),
+            @ApiResponse(responseCode = "404", description = "Client is not found", content = @Content)
+    })
     @PostMapping("/{clientId}/pet/add")
     public ResponseEntity<AbstractNewPetDto> persistPet(@PathVariable("clientId") Long clientId,
                                                         @RequestBody AbstractNewPetDto petDto) {
@@ -90,12 +121,17 @@ public class ClientController {
         return ResponseEntity.notFound().build();
     }
 
-    // DELETE /api/client/pet/{petId} само собой удалить можно только своего питомца
+    @Operation(summary = "delete a Pet")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted the Pet"),
+            @ApiResponse(responseCode = "404", description = "Pet is not found"),
+            @ApiResponse(responseCode = "400", description = "Pet owner ID and url client ID do not match")
+    })
     @DeleteMapping("/{clientId}/pet/{petId}")
     public ResponseEntity<Void> deletePet(@PathVariable("clientId") Long clientId, @PathVariable("petId") Long petId) {
         Pet pet = petService.findById(petId);
         if (pet == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else if (pet.getClient().getId().equals(clientId)) {
             petService.delete(petId);
             return new ResponseEntity<>(HttpStatus.OK);
@@ -104,23 +140,35 @@ public class ClientController {
         }
     }
 
-    // PUT /api/client/pet/{petId} изменение данных питомца
+    @Operation(summary = "update a Pet", description = "Pet requires an existing client. Pet ID before and after must match")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated the Pet"),
+            @ApiResponse(responseCode = "404", description = "Pet or Client is not found"),
+            @ApiResponse(responseCode = "400", description = "Pet owner ID and url client ID do not match")
+    })
     @PutMapping("/{clientId}/pet/{petId}")
     public ResponseEntity<PetDto> updatePet(@PathVariable("clientId") Long clientId, @PathVariable("petId") Long petId,
                                             @RequestBody PetDto petDto) {
         Client client = clientService.getByKey(clientId);
         Pet pet = petService.findById(petId);
         if (client != null && pet != null) {
-            // petDto convert to Pet updatedPet(it's abstract, can't)
-            // petService.update(updatedPet)
-            Pet updatedPet = mapStructMapper.PetDtoToPet(petDto);
-            petService.update(updatedPet);
-            return new ResponseEntity<>(HttpStatus.OK);
+            if (pet.getClient().getId().equals(clientId)) {
+                // petDto convert to Pet updatedPet(it's abstract, can't)
+                // petService.update(updatedPet)
+                Pet updatedPet = mapStructMapper.PetDtoToPet(petDto);
+                petService.update(updatedPet);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    // GET /api/client/pet/{petId}/avatar  получение аватара питомца
+    @Operation(summary = "get avatar of a Pet")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the avatar"),
+            @ApiResponse(responseCode = "404", description = "Client or Pet or Pet's avatar is not found")
+    })
     @GetMapping(value = "/{clientId}/pet/{petId}/avatar")
     public ResponseEntity<byte[]> getPetAvatar(@PathVariable("clientId") Long clientId,
                                                @PathVariable("petId") Long petId) {
@@ -128,10 +176,14 @@ public class ClientController {
         Pet pet = petService.findById(petId);
         if (client != null & pet != null) {
             String url = pet.getAvatar();
-            String filename = StringUtils.substringAfterLast(url, File.separator);
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Content-Type", resourceService.getContentTypeByFileName(filename));
-            return url != null
+            HttpHeaders headers = null;
+            boolean isValidUrl;
+            if (isValidUrl = (url != null)) {
+                String filename = StringUtils.substringAfterLast(url, File.separator);
+                headers = new HttpHeaders();
+                headers.add("Content-Type", resourceService.getContentTypeByFileName(filename));
+            }
+            return isValidUrl
                     ? new ResponseEntity<>(resourceService.loadAsByteArray(url), headers, HttpStatus.OK)
                     : new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
@@ -139,7 +191,12 @@ public class ClientController {
         }
     }
 
-    // POST /api/client/pet/{petId}/avatar  сохранение новой аватарки питомца
+    @Operation(summary = "upload avatar for a Pet")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully uploaded the avatar",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = UploadedFileDto.class))),
+            @ApiResponse(responseCode = "404", description = "Client or Pet is not found", content = @Content)
+    })
     @PostMapping(value = "/{clientId}/pet/{petId}/avatar", consumes = {"multipart/form-data"})
     public ResponseEntity<UploadedFileDto> persistPetAvatar(@PathVariable("clientId") Long clientId,
                                                             @PathVariable("petId") Long petId,
