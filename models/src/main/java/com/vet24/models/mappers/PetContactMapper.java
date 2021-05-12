1aca43640774912851822f36c718867a4064565c
kiranlazarev <kiranlazarev@gmail.com>
2021-05-12 12:32:51 +0200

    model/dao/service for QRCode

---
+++ models/src/main/java/com/vet24/models/mappers/PetContactMapper.java
@@ -0,0 +1,19 @@
+package com.vet24.models.mappers;
+
+import com.vet24.models.dto.contact.PetContactDto;
+import com.vet24.models.pet.PetContact;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PetContactMapper {
+
+    PetContact petContactDtoToPetContact(PetContactDto petContactDto);
+
+    PetContactDto petContactToPetContactDto(PetContact petContact);
+
+    List<PetContact> petContactDtoListToPetContact(List<PetContactDto> petContactDtoList);
+
+    List<PetContactDto> petContactListToPetContactDto(List<PetContact> petContactList);
+}
