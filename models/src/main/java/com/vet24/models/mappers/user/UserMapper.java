481b9fcfdc003c4dd421b1190a94543ea0e6a52c
Cloud <ruwsps@gmail.com>
2021-05-13 14:01:27 +0300

    fixing Mappers in progress

--- models/src/main/java/com/vet24/models/mappers/user/UserMapper.java
+++ models/src/main/java/com/vet24/models/mappers/user/UserMapper.java
@@ -1,11 +1,14 @@
 package com.vet24.models.mappers.user;
 
 import com.vet24.models.dto.user.ClientDto;
+import com.vet24.models.mappers.pet.PetMapper;
 import com.vet24.models.user.Client;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {PetMapper.class})
+@Component
 public interface UserMapper {
 
     @Mapping(source = "login", target = "username")
