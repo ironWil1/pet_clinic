c23326d7f20d8a676c85262e39aff8e3b436831e
kiranlazarev <kiranlazarev@gmail.com>
2021-05-19 13:13:42 +0200

    test

--- web/src/test/java/com/vet24/web/controllers/qrcode/PetContactQrCodeControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/qrcode/PetContactQrCodeControllerTest.java
@@ -18,24 +18,26 @@ public class PetContactQrCodeControllerTest extends ControllerAbstractIntegratio
 
     final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
-    @Autowired
-    private PetContactQrCodeController petContactQrCodeController;
-
+    // get create qr code for petContact by id - success
     @Test
-    public void createQrCodeForViewOnePetContactOrNotFoundIdForPetContact() throws Exception {
+    public void testCreateQrCodeForViewOnePetContactSuccess() throws Exception {
         final String URL_GET_PET_CONTACT = "/api/client/pet/1/qr";
         this.mockMvc.perform(MockMvcRequestBuilders.get(URL_GET_PET_CONTACT)).andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(MockMvcResultHandlers.print());
-
-        final String URL_GET_NOT_FOUND_ID_PET_CONTACT = "/api/client/pet/1000/qr";
-        this.mockMvc.perform(MockMvcRequestBuilders.get(URL_GET_NOT_FOUND_ID_PET_CONTACT))
-                .andExpect(MockMvcResultMatchers.status().isNotFound())
-                .andDo(MockMvcResultHandlers.print());
-
     }
 
+    // get pet by id - error 404 pet not found
     @Test
-    public void updateOldAndSaveNewPetContactForPetOrNotFoundIdForPet() throws Exception {
+    public void testCreateQrCodeForViewOnePetContactError404Pet() throws Exception {
+        final String URL_GET_NOT_FOUND_ID_PET = "/api/client/pet/1000/qr";
+        this.mockMvc.perform(MockMvcRequestBuilders.get(URL_GET_NOT_FOUND_ID_PET))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    // post update petContact by id - success
+    @Test
+    public void testUpdatePetContactForPetSuccess() throws Exception {
         final String URL_POST_UPDATE = "/api/client/pet/1/qr";
         PetContactDto petContact1 = new PetContactDto("Мария", "Невского 17", "4854789899");
         String bodyUpdate = (new ObjectMapper()).valueToTree(petContact1).toString();
@@ -43,7 +45,11 @@ public class PetContactQrCodeControllerTest extends ControllerAbstractIntegratio
                 .content(bodyUpdate).contentType(APPLICATION_JSON_UTF8))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andDo(MockMvcResultHandlers.print());
+    }
 
+    // post create petContact by id - success
+    @Test
+    public void testCreatePetContactForPetSuccess() throws Exception {
         final String URL_POST_CREATE = "/api/client/pet/7/qr";
         PetContactDto petContact2 = new PetContactDto("Мария", "Невского 17", "5647564343");
         String bodyCreate = (new ObjectMapper()).valueToTree(petContact2).toString();
@@ -51,13 +57,18 @@ public class PetContactQrCodeControllerTest extends ControllerAbstractIntegratio
                 .content(bodyCreate).contentType(APPLICATION_JSON_UTF8))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andDo(MockMvcResultHandlers.print());
+    }
 
-        final String URL_POST_NOT_FOUND_ID = "/api/client/pet/1000/qr";
+    // post pet by id - error 404 pet not found
+    @Test
+    public void testUpdateAndCreatePetContactForPetError404Pet() throws Exception {
+        final String URL_POST_NOT_FOUND_ID_PET = "/api/client/pet/1000/qr";
         PetContactDto petContact3 = new PetContactDto("Мария", "Невского 17", "2456786957");
         String body = (new ObjectMapper()).valueToTree(petContact3).toString();
-        this.mockMvc.perform(MockMvcRequestBuilders.post(URL_POST_NOT_FOUND_ID)
+        this.mockMvc.perform(MockMvcRequestBuilders.post(URL_POST_NOT_FOUND_ID_PET)
                 .content(body).contentType(APPLICATION_JSON_UTF8))
                 .andExpect(MockMvcResultMatchers.status().isNotFound())
                 .andDo(MockMvcResultHandlers.print());
     }
+
 }
