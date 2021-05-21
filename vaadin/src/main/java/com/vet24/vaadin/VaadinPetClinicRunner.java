f1bf7552a37957a15e62de670ba07ad805a3cf95
Artem Kulin <kulinartem@gmail.com>
2021-05-21 23:05:08 +0300

    Add configurations files

---
+++ vaadin/src/main/java/com/vet24/vaadin/VaadinPetClinicRunner.java
@@ -0,0 +1,14 @@
+package com.vet24.vaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:application.properties")
+public class VaadinPetClinicRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinPetClinicRunner.class, args);
+    }
+}
