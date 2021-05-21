d48f6e722655b6f2311d9146b51e2252bf91420a
VeraPolyanskikh <vpcat3@gmail.com>
2021-05-21 19:17:04 +0300

    impl and refactoring login => email; Exception handler logic simplified;

---
+++ models/src/main/java/com/vet24/models/user/VerificationToken.java
@@ -0,0 +1,26 @@
+package com.vet24.models.user;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class VerificationToken {
+    @Id
+    @NonNull
+    private Long id;
+
+    @NonNull
+    @OneToOne(targetEntity = Client.class,cascade =
+            {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REFRESH},
+            fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "user_id")
+    private Client client;
+
+}
