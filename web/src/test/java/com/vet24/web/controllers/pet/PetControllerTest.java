1fe21f35a0156ca169f0b849636d606126590d5d
Cloud <ruwsps@gmail.com>
2021-05-22 12:40:20 +0300

    more tests for PetControllerTest

--- web/src/test/java/com/vet24/web/controllers/pet/PetControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/pet/PetControllerTest.java
@@ -9,11 +9,7 @@ import com.vet24.models.dto.pet.PetDto;
 import com.vet24.models.enums.Gender;
 import com.vet24.models.enums.PetSize;
 import com.vet24.models.enums.PetType;
-import com.vet24.models.enums.RoleNameEnum;
-import com.vet24.models.pet.Dog;
 import com.vet24.models.pet.Pet;
-import com.vet24.models.user.Client;
-import com.vet24.models.user.Role;
 import com.vet24.web.ControllerAbstractIntegrationTest;
 import org.junit.Assert;
 import org.junit.Before;
@@ -29,7 +25,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 
 import java.time.LocalDate;
-import java.util.HashSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -44,19 +39,12 @@ public class PetControllerTest extends ControllerAbstractIntegrationTest {
 
     private final String URI = "http://localhost:8090/api/client/pet";
 
-    private Client client;
-
-    private Pet pet;
-
     private AbstractNewPetDto abstractNewPetDto;
 
     @Before
     public void createNewClientAndDog() {
-        this.client = new Client("firstname", "lastname", "email@email.com", "password",
-                new Role(RoleNameEnum.CLIENT), new HashSet<>());
-        this.pet = new Dog("name", LocalDate.now(), Gender.MALE, "breed", client);
-        this.abstractNewPetDto = new DogDto("name", PetType.DOG, LocalDate.now(), Gender.MALE, "breed", "color",
-                PetSize.MEDIUM, 9.3, "description", "test.png", 0);
+        this.abstractNewPetDto = new DogDto("name", PetType.DOG, LocalDate.now(), Gender.MALE, "breed",
+                "color", PetSize.MEDIUM, 9.3, "description", "test.png", 0);
     }
 
     @Test
@@ -94,6 +82,36 @@ public class PetControllerTest extends ControllerAbstractIntegrationTest {
 
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
+    public void deletePetOfAnotherOwnerBadRequest() {
+        int sizeBefore = petDao.getAll().size();
+        HttpHeaders headers = new HttpHeaders();
+        ResponseEntity<Void> response = testRestTemplate
+                .exchange(URI + "/{petId}", HttpMethod.DELETE, new HttpEntity<>(headers), Void.class, 100);
+        int sizeAfter = petDao.getAll().size();
+        Pet pet = petDao.getByKey(100L);
+
+        Assert.assertNotNull(pet);
+        Assert.assertEquals(sizeBefore, sizeAfter);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
+    public void deletePetThatDoesNotExistNotFound() {
+        Pet petBefore = petDao.getByKey(69000L);
+        int sizeBefore = petDao.getAll().size();
+        HttpHeaders headers = new HttpHeaders();
+        ResponseEntity<Void> response = testRestTemplate
+                .exchange(URI + "/{petId}", HttpMethod.DELETE, new HttpEntity<>(headers), Void.class, 69000);
+        int sizeAfter = petDao.getAll().size();
+
+        Assert.assertNull(petBefore);
+        Assert.assertEquals(sizeBefore, sizeAfter);
+        Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
     public void updatePetSuccess() {
         Pet petBefore = petDao.getByKey(102L);
         int sizeBefore = petDao.getAll().size();
@@ -110,12 +128,55 @@ public class PetControllerTest extends ControllerAbstractIntegrationTest {
 
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
+    public void updatePetOfAnotherOwnerBadRequest() {
+        Pet petBefore = petDao.getByKey(100L);
+        int sizeBefore = petDao.getAll().size();
+        HttpEntity<AbstractNewPetDto> request = new HttpEntity<>(abstractNewPetDto, new HttpHeaders());
+        ResponseEntity<PetDto> response = testRestTemplate
+                .exchange(URI + "/{petId}", HttpMethod.PUT, request, PetDto.class, 100);
+        Pet petAfter = petDao.getByKey(100L);
+        int sizeAfter = petDao.getAll().size();
+
+        Assert.assertEquals(sizeBefore, sizeAfter);
+        Assert.assertEquals(petBefore.getId(), petAfter.getId());
+        Assert.assertEquals(petBefore.getPetType(), petAfter.getPetType());
+        Assert.assertEquals(petBefore.getName(), petAfter.getName());
+        Assert.assertEquals(petBefore.getBirthDay(), petAfter.getBirthDay());
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
+    public void updatePetThatDoesNotExistNotFound() {
+        Pet petBefore = petDao.getByKey(69000L);
+        int sizeBefore = petDao.getAll().size();
+        HttpEntity<AbstractNewPetDto> request = new HttpEntity<>(abstractNewPetDto, new HttpHeaders());
+        ResponseEntity<PetDto> response = testRestTemplate
+                .exchange(URI + "/{petId}", HttpMethod.PUT, request, PetDto.class, 69000);
+        int sizeAfter = petDao.getAll().size();
+
+        Assert.assertNull(petBefore);
+        Assert.assertEquals(sizeBefore, sizeAfter);
+        Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
     public void getPetAvatarSuccess() {
         persistPetAvatarSuccess();
         ResponseEntity<byte[]> response = testRestTemplate
                 .getForEntity(URI + "/{petId}/avatar", byte[].class, 102);
 
         Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
+    public void getPetAvatarButPetDoesNotExistNotFound() {
+        ResponseEntity<byte[]> response = testRestTemplate
+                .getForEntity(URI + "/{petId}/avatar", byte[].class, 69000);
+
+        Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
@@ -130,5 +191,33 @@ public class PetControllerTest extends ControllerAbstractIntegrationTest {
                 .exchange(URI + "/{petId}/avatar", HttpMethod.POST, entity, String.class, 102);
 
         Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
+    public void persistPetAvatarOfAnotherOwnerBadRequest() {
+        LinkedMultiValueMap<String, Object> parameters = new LinkedMultiValueMap<>();
+        parameters.add("file", new ClassPathResource("test.png"));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<LinkedMultiValueMap<String, Object>> entity = new HttpEntity<>(parameters, headers);
+        ResponseEntity<String> response = testRestTemplate
+                .exchange(URI + "/{petId}/avatar", HttpMethod.POST, entity, String.class, 100);
+
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
+    public void persistPetAvatarButPetDoesNotExistNotFound() {
+        LinkedMultiValueMap<String, Object> parameters = new LinkedMultiValueMap<>();
+        parameters.add("file", new ClassPathResource("test.png"));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<LinkedMultiValueMap<String, Object>> entity = new HttpEntity<>(parameters, headers);
+        ResponseEntity<String> response = testRestTemplate
+                .exchange(URI + "/{petId}/avatar", HttpMethod.POST, entity, String.class, 69000);
+
+        Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 }
