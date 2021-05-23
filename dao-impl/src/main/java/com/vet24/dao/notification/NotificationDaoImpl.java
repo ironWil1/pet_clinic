67cf7973aabe4a758be2de008481a9482092536a
Polina Kolzunova <polinakolzunova@gmail.com>
2021-05-23 09:54:59 +0300

    add service & repository annotations

--- dao-impl/src/main/java/com/vet24/dao/notification/NotificationDaoImpl.java
+++ dao-impl/src/main/java/com/vet24/dao/notification/NotificationDaoImpl.java
@@ -2,6 +2,8 @@ package com.vet24.dao.notification;
 
 import com.vet24.dao.ReadWriteDaoImpl;
 import com.vet24.models.notification.Notification;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class NotificationDaoImpl extends ReadWriteDaoImpl<Long, Notification> implements NotificationDao {
 }
--- service-impl/src/main/java/com/vet24/service/notification/NotificationServiceImpl.java
+++ service-impl/src/main/java/com/vet24/service/notification/NotificationServiceImpl.java
@@ -4,7 +4,9 @@ import com.vet24.dao.ReadWriteDaoImpl;
 import com.vet24.dao.notification.NotificationDao;
 import com.vet24.models.notification.Notification;
 import com.vet24.service.ReadWriteServiceImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class NotificationServiceImpl extends ReadWriteServiceImpl<Long, Notification> implements NotificationService {
 
     private final NotificationDao notificationDao;
