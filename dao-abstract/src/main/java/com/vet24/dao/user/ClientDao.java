7fe419a80113f49fc267379442f833e0da44c012
Cloud <ruwsps@gmail.com>
2021-05-24 13:10:53 +0300

    parameter name fix

--- dao-abstract/src/main/java/com/vet24/dao/user/ClientDao.java
+++ dao-abstract/src/main/java/com/vet24/dao/user/ClientDao.java
@@ -7,7 +7,7 @@ import com.vet24.models.user.Client;
 
 public interface ClientDao extends ReadWriteDao<Long, Client> {
 
-    Client getClientByEmail(String login);
+    Client getClientByEmail(String email);
 
     Client testGetCurrentClientEagerly();
 }
