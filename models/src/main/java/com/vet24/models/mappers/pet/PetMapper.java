9e2d6ac87ed2e36909f52b984b61f58c3d76a8e9
Artem Kulin <kulinartem@gmail.com>
2021-05-28 14:54:48 +0300

    Modify Pet Mapper: refactor switch case

--- models/src/main/java/com/vet24/models/mappers/pet/PetMapper.java
+++ models/src/main/java/com/vet24/models/mappers/pet/PetMapper.java
@@ -1,39 +1,34 @@
 package com.vet24.models.mappers.pet;
 
 import com.vet24.models.dto.pet.AbstractNewPetDto;
-import com.vet24.models.dto.pet.CatDto;
-import com.vet24.models.dto.pet.DogDto;
 import com.vet24.models.dto.pet.PetDto;
+import com.vet24.models.enums.PetType;
 import com.vet24.models.pet.Pet;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Mapper(componentModel = "spring")
 public abstract class PetMapper {
 
-    @Autowired
-    private DogMapper dogMapper;
+    Map<PetType, AbstractPetMapper> mapperMap;
 
     @Autowired
-    private CatMapper catMapper;
+    public void setMapperMap(List<AbstractPetMapper> mapperList) {
+        mapperMap = mapperList.stream().collect(Collectors.toMap(AbstractPetMapper::getPetType, Function.identity()));
+    }
 
     @Mapping(source = "petType", target = "type")
     public abstract PetDto petToPetDto(Pet pet);
 
     public Pet abstractNewPetDtoToPet(AbstractNewPetDto petDto) {
-        Pet pet = null;
-        String petType = petDto.getPetType().name();
-        switch (petType) {
-            case "DOG":
-                pet = dogMapper.dogDtoToDog((DogDto) petDto);
-                break;
-            case "CAT":
-                pet = catMapper.catDtoToCat((CatDto) petDto);
-                break;
-            default:
-                break;
-        }
-        return pet;
+        return mapperMap.containsKey(petDto.getPetType()) ?
+                mapperMap.get(petDto.getPetType()).transferAbstractPetDto(petDto) :
+                null;
     }
 }
