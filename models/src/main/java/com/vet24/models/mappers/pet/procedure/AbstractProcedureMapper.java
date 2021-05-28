d31d600ac497aae03a36553398144af3aac2741a
Artem Kulin <kulinartem@gmail.com>
2021-05-28 23:55:11 +0300

    Add abstract interface for procedure Mappers

---
+++ models/src/main/java/com/vet24/models/mappers/pet/procedure/AbstractProcedureMapper.java
@@ -0,0 +1,12 @@
+package com.vet24.models.mappers.pet.procedure;
+
+import com.vet24.models.dto.pet.procedure.AbstractNewProcedureDto;
+import com.vet24.models.dto.pet.procedure.ProcedureDto;
+import com.vet24.models.enums.ProcedureType;
+import com.vet24.models.pet.procedure.Procedure;
+
+public interface AbstractProcedureMapper {
+    ProcedureType getType();
+    Procedure transferAbstractProcedureDto (AbstractNewProcedureDto abstractNewProcedureDto);
+    Procedure transferProcedureDto (ProcedureDto procedureDto);
+}
