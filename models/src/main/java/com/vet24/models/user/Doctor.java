7d59d058d52d65ff0d04b629c7775d0af2a8aeb9
VeraPolyanskikh <vpcat3@gmail.com>
2021-05-31 17:25:15 +0300

    impl; exception handler simplified

--- models/src/main/java/com/vet24/models/user/Doctor.java
+++ models/src/main/java/com/vet24/models/user/Doctor.java
@@ -1,16 +1,24 @@
 package com.vet24.models.user;
 
 import com.vet24.models.medicine.Diagnosis;
+<<<<<<< HEAD
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+=======
+import lombok.*;
+>>>>>>> impl; exception handler simplified
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
 @DiscriminatorValue("DOCTOR")
-@EqualsAndHashCode(callSuper = true,exclude = "comments,diagnoses")
+@EqualsAndHashCode(callSuper = true,exclude = "diagnoses")
 @AllArgsConstructor
 public class Doctor extends User {
 
@@ -23,13 +31,6 @@ public class Doctor extends User {
             cascade = CascadeType.ALL
     )
     private Set<Diagnosis> diagnoses = new HashSet<>();
-
-    @OneToMany(
-            mappedBy = "doctor",
-            orphanRemoval = true,
-            cascade = CascadeType.ALL
-    )
-    private List<Comment> comments;
 
     public Doctor() {
         super();
