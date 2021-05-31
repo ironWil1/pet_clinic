f8c12974b03d83c7a152b9eccbd2e632f34adbee
VeraPolyanskikh <vpcat3@gmail.com>
2021-05-31 18:12:45 +0300

    for example

--- web/src/test/java/com/vet24/web/controllers/user/DoctorControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/user/DoctorControllerTest.java
@@ -3,16 +3,24 @@ package com.vet24.web.controllers.user;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
 import com.vet24.models.dto.medicine.DiagnosisDto;
+import com.vet24.models.dto.user.DoctorDto;
 import com.vet24.models.mappers.user.DoctorMapper;
+import com.vet24.models.medicine.Diagnosis;
 import com.vet24.models.user.Doctor;
 import com.vet24.service.pet.PetService;
 import com.vet24.service.user.DoctorService;
 import com.vet24.web.ControllerAbstractIntegrationTest;
 import com.vet24.web.controllers.medicine.DoctorController;
+import org.hibernate.Hibernate;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @DBRider
 public class DoctorControllerTest extends ControllerAbstractIntegrationTest {
@@ -54,12 +62,25 @@ public class DoctorControllerTest extends ControllerAbstractIntegrationTest {
                 .exchange(DOMAIN+DoctorController.ADD_DIAGNOSIS_API, HttpMethod.POST,entity, DiagnosisDto.class,101L);
         Assert.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
         Assert.assertEquals("bla-bla-bla",responseEntity.getBody().getDescription());
-        Assert.assertEquals(doctor.getId(),responseEntity.getBody().getDoctorId());
+       // Assert.assertEquals(doctor.getId(),responseEntity.getBody().getDoctor().getId());
         Assert.assertEquals(petService.getByKey(101L).getId(),responseEntity.getBody().getPet().getId());
         Assert.assertNotNull(responseEntity.getBody().getId());
-        Assert.assertEquals(responseEntity.getBody().getDoctorId(),doctor.getId());
 
+
+       // Hibernate.initialize(doctor.getDiagnoses());
+        DoctorDto doctorDto = doctorMapper.doctorToDoctorDto(doctor);
+       // PersistenceUtil persistenceUnitUtil = Persistence.getPersistenceUtil();
+        Assert.assertEquals(responseEntity.getBody().getDoctor().getDiagnoses().size(),1);
+     Assert.assertEquals(doctorDto,responseEntity.getBody().getDoctor());
+        Assert.assertEquals(doctorDto.getDiagnoses(),responseEntity.getBody().getDoctor().getDiagnoses());
 
     }
+
+    private HttpHeaders createHeaders(String username, String password){
+        return new HttpHeaders() {{
+            setBasicAuth(username,password);
+        }};
+    }
+
 
 }
