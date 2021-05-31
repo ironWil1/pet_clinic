8874d371f7e907f01f1584c1fdcd6700560ba9a7
Artem Kulin <kulinartem@gmail.com>
2021-05-31 21:19:15 +0300

    Add CommentDaoImpl

---
+++ dao-impl/src/main/java/com/vet24/dao/user/CommentDaoImpl.java
@@ -0,0 +1,9 @@
+package com.vet24.dao.user;
+
+import com.vet24.dao.ReadWriteDaoImpl;
+import com.vet24.models.user.Comment;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CommentDaoImpl extends ReadWriteDaoImpl<Long, Comment>  implements CommentDao{
+}
