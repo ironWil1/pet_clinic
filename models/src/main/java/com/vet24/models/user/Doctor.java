850667bdd16e824eb82c2d40d8e40d464bb2a20c
Polina Kolzunova <polinakolzunova@gmail.com>
2021-06-01 00:25:28 +0300

    delete unused imports

--- models/src/main/java/com/vet24/models/user/Doctor.java
+++ models/src/main/java/com/vet24/models/user/Doctor.java
@@ -1,14 +1,10 @@
 package com.vet24.models.user;
 
-import com.vet24.models.medicine.Diagnosis;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Data
