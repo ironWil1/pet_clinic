3d77cdb20868e81f981528b511d2863ff0fa077b
vera_polyanskikh <Zambezi33>
2021-06-08 17:33:14 +0300

    impl via @IdClass ; fixed data model for Comment ( extra foreign key removed; LocalDate switched to LocalDatetime)

--- models/src/main/java/com/vet24/models/user/Like.java
+++ models/src/main/java/com/vet24/models/user/Like.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @EqualsAndHashCode
 @AllArgsConstructor
-@Table(name="commentLike")
+@Table(name="comment_like")
 @IdClass(LikeId.class)
 public class Like {
 
