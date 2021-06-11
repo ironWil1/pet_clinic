a50a4f52fec469bdcb2424edc3a6ae8f8540852a
moms-array <khalyavinartem@yandex.ru>
2021-06-11 21:48:08 +0300

    изменил class на interface заменил реализацию mapper на аннотации удалил лишние импорты

--- models/src/main/java/com/vet24/models/mappers/medicine/TreatmentMapper.java
+++ models/src/main/java/com/vet24/models/mappers/medicine/TreatmentMapper.java
@@ -2,24 +2,14 @@ package com.vet24.models.mappers.medicine;
 
 import com.vet24.models.dto.medicine.TreatmentDto;
 import com.vet24.models.medicine.Treatment;
-import com.vet24.models.pet.procedure.Procedure;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.util.stream.Collectors;
-
 @Mapper(componentModel = "spring")
-public class TreatmentMapper {
+public interface TreatmentMapper {
 
-    public TreatmentDto treatmentToTreatmentDto(Treatment treatment){
-        TreatmentDto treatmentDto = new TreatmentDto();
-        treatmentDto.setProcedureSetId(treatment.getProcedureSet()
-                .stream()
-                .map(Procedure::getId)
-                .collect(Collectors.toSet()));
-        treatmentDto.setDoctorId(treatment.getDiagnosis().getDoctor().getId());
-        treatmentDto.setId(treatment.getId());
-        treatmentDto.setDiagnosisId(treatment.getDiagnosis().getId());
-        return treatmentDto;
-    }
+    @Mapping(source = "diagnosis.id", target = "diagnosisId")
+    @Mapping(source = "diagnosis.doctor.id", target = "doctorId")
+    @Mapping(source = "procedureList", target = "procedureDtoList")
+    TreatmentDto treatmentToTreatmentDto(Treatment treatment);
 }
