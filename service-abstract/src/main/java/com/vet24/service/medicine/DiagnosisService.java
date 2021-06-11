63a6c58d311afb7cc3f6def45dfa9d3534060307
moms-array <khalyavinartem@yandex.ru>
2021-06-11 21:41:39 +0300

    косметика

--- service-abstract/src/main/java/com/vet24/service/medicine/DiagnosisService.java
+++ service-abstract/src/main/java/com/vet24/service/medicine/DiagnosisService.java
@@ -1,9 +1,8 @@
 package com.vet24.service.medicine;
 
 import com.vet24.models.medicine.Diagnosis;
+import com.vet24.service.ReadWriteService;
 
-public interface DiagnosisService {
-
-
+public interface DiagnosisService extends ReadWriteService<Long,Diagnosis> {
     void persist(Diagnosis diagnosis);
 }
