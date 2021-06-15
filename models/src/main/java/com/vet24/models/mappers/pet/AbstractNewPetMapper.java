73aadb81cbbe5ee7ce86cfe82330bec876ab571a
svubax <svubax@ya.ru>
2021-06-15 21:55:18 +0300

    add mapper interface 3

--- models/src/main/java/com/vet24/models/mappers/pet/AbstractNewPetMapper.java
+++ models/src/main/java/com/vet24/models/mappers/pet/AbstractNewPetMapper.java
@@ -1,13 +1,11 @@
 package com.vet24.models.mappers.pet;
 
-import com.vet24.models.dto.pet.PetDto;
+import com.vet24.models.dto.pet.AbstractNewPetDto;
 import com.vet24.models.enums.PetType;
 import com.vet24.models.exception.NoSuchAbstractEntityDtoException;
 import com.vet24.models.mappers.DtoMapper;
-import com.vet24.models.mappers.EntityMapper;
 import com.vet24.models.pet.Pet;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
@@ -17,8 +15,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
-public abstract class PetMapper implements
-        DtoMapper<Pet, PetDto>, EntityMapper<PetDto, Pet> {
+public abstract class AbstractNewPetMapper implements DtoMapper<Pet, AbstractNewPetDto> {
 
     private Map<PetType, AbstractPetMapper> mapperMap;
 
@@ -34,16 +31,11 @@ public abstract class PetMapper implements
         mapperMap = mapperList.stream().collect(Collectors.toMap(AbstractPetMapper::getPetType, Function.identity()));
     }
 
-    @Mapping(source = "petType", target = "type")
-    @Override
-    public abstract PetDto toDto(Pet pet);
-
-    @Override
-    public Pet toEntity (PetDto petDto) {
-        if (mapperMap.containsKey(petDto.getType())) {
-            return mapperMap.get(petDto.getType()).abstractPetDtoToPet(petDto);
+    public Pet toEntity(AbstractNewPetDto abstractNewPetDto) {
+        if (mapperMap.containsKey(abstractNewPetDto.getPetType())) {
+            return mapperMap.get(abstractNewPetDto.getPetType()).abstractNewPetDtoToPet(abstractNewPetDto);
         } else {
-            throw new NoSuchAbstractEntityDtoException("Can't find Mapper for " + petDto);
+            throw new NoSuchAbstractEntityDtoException("Can't find Mapper for " + abstractNewPetDto);
         }
     }
 }
