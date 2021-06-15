ca7b338749906fe3f15202bd681a9b88fa59be77
moms-array <khalyavinartem@yandex.ru>
2021-06-15 12:35:44 +0300

    1) в контроллер saveOrUpdatePetContact добавил частичное обновление petContact. 2) в контроллер createPetContactQrCode добавил:  - проверку принадлежности Peta текущему Client  - если нет petContact то она создается из имеющихся в наличии данных, генерируется QR code и отправляется в response  - добавил еще одну аннотацию @ApiResponse

--- web/src/main/java/com/vet24/web/controllers/qrcode/PetContactQrCodeController.java
+++ web/src/main/java/com/vet24/web/controllers/qrcode/PetContactQrCodeController.java
@@ -4,8 +4,10 @@ import com.vet24.models.dto.pet.PetContactDto;
 import com.vet24.models.mappers.pet.PetContactMapper;
 import com.vet24.models.pet.Pet;
 import com.vet24.models.pet.PetContact;
+import com.vet24.models.user.Client;
 import com.vet24.service.pet.PetContactService;
 import com.vet24.service.pet.PetService;
+import com.vet24.service.user.ClientService;
 import com.vet24.util.qrcode.PetContactQrCodeGenerator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.webjars.NotFoundException;
 
 @RestController
 @RequestMapping("/api/client/pet")
@@ -28,32 +31,60 @@ public class PetContactQrCodeController {
     private final PetService petService;
     private final PetContactService petContactService;
     private final PetContactMapper petContactMapper;
+    private final ClientService clientService;
 
-    public PetContactQrCodeController(PetService petService, PetContactService petContactService, PetContactMapper petContactMapper) {
+    public PetContactQrCodeController(PetService petService, PetContactService petContactService, PetContactMapper petContactMapper, ClientService clientService) {
         this.petService = petService;
         this.petContactService = petContactService;
         this.petContactMapper = petContactMapper;
+        this.clientService = clientService;
     }
 
     @Operation(summary = "Encode and create qr code for pet contact")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully create the qr code", content = @Content()),
             @ApiResponse(responseCode = "404", description = "PetContact ID is not found"),
+            @ApiResponse(responseCode = "404", description = "Pet is not yours")
     })
     @GetMapping(value = "/{id}/qr", produces = MediaType.IMAGE_PNG_VALUE)
     public ResponseEntity<byte[]> createPetContactQrCode(@PathVariable("id") Long id) {
+        if (!clientService.getCurrentClient().getPets().contains(petService.getByKey(id))){
+            throw new NotFoundException("It's pet is not yours");
+        }
         if (petContactService.isExistByKey(id)) {
             PetContact petContact = petContactService.getByKey(id);
             String UrlToAlertPetContact = "/api/petFound?petCode=" + petContact.getPetCode();
             String sb = "Имя питомца - " + petContact.getPet().getName() + ", " +
                     "Владелец - " + petContact.getOwnerName() + ", " +
                     "Адрес - " + petContact.getAddress() + ", " +
-                    "Телефон - " + petContact.getPhone() + ". " +
+                    "Телефон - " + petContact.getPhone() + ", " +
                     "Чтобы сообщить владельцу о находке перейдите по адресу - " + UrlToAlertPetContact;
             return ResponseEntity.ok(PetContactQrCodeGenerator.generatePetContactQrCodeImage(sb));
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else if(petService.isExistByKey(id)) {
+            Pet pet = petService.getByKey(id);
+            Client client = pet.getClient();
+            PetContact petContact = new PetContact();
+            petContact.setAddress("unknown");
+            petContact.setOwnerName(pet.getClient().getFirstname());
+            petContact.setPetCode(petContactService.randomPetContactUniqueCode(id));
+            petContact.setPhone(pet.getId());
+            petContact.setPet(pet);
+            //
+            //номер находится в карточке petContact, в клиенте нет номера, стоит пока заглушка в виде id
+            //адрес находится в карточке petContact, в клиенте нет адреса, стоит заглушка в виде unknown
+            petContactService.persist(petContact);
+
+            String urlToAlertPetContact = "/api/petFound?petCode=" + petContact.getPetCode();
+            String sb = "Имя питомца - " + pet.getName() + ", " +
+                    "Владелец - " + client.getFirstname() + ", " +
+                    "Адрес - " + (petContact.getAddress().equals("unknown") ? "владелец еще не успел указать адрес" : petContact.getAddress()) + ", " +
+                    "Телефон - " + (petContact.getPhone().toString().length() < 11 ? "владелец не успел указать номер телефона" : petContact.getPhone()) + ", " +
+                    "Почта - " + client.getEmail() + ", " +
+                    "чтобы сообщить владельцу о находке перейдите по адресу - " + urlToAlertPetContact;
+            return new ResponseEntity<>(PetContactQrCodeGenerator.generatePetContactQrCodeImage(sb),
+                    HttpStatus.OK);
         }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @Operation(summary = "Create and update PetContact for qr code")
@@ -69,8 +100,17 @@ public class PetContactQrCodeController {
     public ResponseEntity<PetContactDto> saveOrUpdatePetContact(@RequestBody PetContactDto petContactDto,
                                                                 @PathVariable("id") Long id) {
         if (petContactService.isExistByKey(id)) {
-            PetContact petContactNew = petContactMapper.petContactDtoToPetContact(petContactDto);
             PetContact petContactOld = petContactService.getByKey(id);
+            if(petContactDto.getOwnerName().equals("")){
+                petContactDto.setOwnerName(petContactOld.getOwnerName());
+            }
+            if(petContactDto.getAddress().equals("")){
+                petContactDto.setAddress(petContactOld.getAddress());
+            }
+            if(petContactDto.getPhone().equals("")){
+                petContactDto.setPhone(petContactOld.getPhone().toString());
+            }
+            PetContact petContactNew = petContactMapper.petContactDtoToPetContact(petContactDto);
             petContactOld.setOwnerName(petContactNew.getOwnerName());
             petContactOld.setAddress(petContactNew.getAddress());
             petContactOld.setPhone(petContactNew.getPhone());
