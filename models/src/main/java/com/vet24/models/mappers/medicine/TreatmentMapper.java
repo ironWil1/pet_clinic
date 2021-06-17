574d3849802f3e29633fd0165d6a2b9f619a858c
moms-array <khalyavinartem@yandex.ru>
2021-06-17 10:46:14 +0300

    - добавилл deafault метод для преобразования list list<NewAbstratProcedureDto> в lsit<Procedure>

--- models/src/main/java/com/vet24/models/mappers/medicine/TreatmentMapper.java
+++ models/src/main/java/com/vet24/models/mappers/medicine/TreatmentMapper.java
@@ -1,7 +1,9 @@
 package com.vet24.models.mappers.medicine;
 
 import com.vet24.models.dto.medicine.TreatmentDto;
+import com.vet24.models.dto.pet.procedure.ProcedureDto;
 import com.vet24.models.medicine.Treatment;
+import com.vet24.models.pet.procedure.Procedure;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -12,4 +14,15 @@ public interface TreatmentMapper {
     @Mapping(source = "diagnosis.doctor.id", target = "doctorId")
     @Mapping(source = "procedureList", target = "procedureDtoList")
     TreatmentDto treatmentToTreatmentDto(Treatment treatment);
+
+    default ProcedureDto toProcedureDto(Procedure procedure){
+        return new ProcedureDto(procedure.getId(),
+                procedure.getDate(),
+                procedure.getType(),
+                procedure.getMedicine().getId(),
+                procedure.getMedicineBatchNumber(),
+                procedure.getIsPeriodical(),
+                procedure.getPeriodDays());
+    }
+
 }
