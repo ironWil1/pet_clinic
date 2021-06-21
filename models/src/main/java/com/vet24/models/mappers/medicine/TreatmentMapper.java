6a1e888fb795634d5f3e9427c4e7e53093a72a4e
moms-array <khalyavinartem@yandex.ru>
2021-06-21 19:16:00 +0300

    - удалил метод toProcedureDto, вместо него в аннотации Mapper прописал uses = {ProcedureMapperImpl.class}

--- models/src/main/java/com/vet24/models/mappers/medicine/TreatmentMapper.java
+++ models/src/main/java/com/vet24/models/mappers/medicine/TreatmentMapper.java
@@ -1,28 +1,19 @@
 package com.vet24.models.mappers.medicine;
 
 import com.vet24.models.dto.medicine.TreatmentDto;
-import com.vet24.models.dto.pet.procedure.ProcedureDto;
+import com.vet24.models.mappers.DtoMapper;
+import com.vet24.models.mappers.EntityMapper;
+import com.vet24.models.mappers.pet.procedure.ProcedureMapperImpl;
 import com.vet24.models.medicine.Treatment;
-import com.vet24.models.pet.procedure.Procedure;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
-public interface TreatmentMapper {
+@Mapper(componentModel = "spring", uses = {ProcedureMapperImpl.class})
+public interface TreatmentMapper extends DtoMapper<Treatment,TreatmentDto>, EntityMapper<TreatmentDto, Treatment> {
 
+    @Override
     @Mapping(source = "diagnosis.id", target = "diagnosisId")
     @Mapping(source = "diagnosis.doctor.id", target = "doctorId")
     @Mapping(source = "procedureList", target = "procedureDtoList")
-    TreatmentDto treatmentToTreatmentDto(Treatment treatment);
-
-    default ProcedureDto toProcedureDto(Procedure procedure){
-        return new ProcedureDto(procedure.getId(),
-                procedure.getDate(),
-                procedure.getType(),
-                procedure.getMedicine().getId(),
-                procedure.getMedicineBatchNumber(),
-                procedure.getIsPeriodical(),
-                procedure.getPeriodDays());
-    }
-
+    TreatmentDto toDto(Treatment treatment);
 }
