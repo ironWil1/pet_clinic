dc238397c0cccf068bba28d94e248f40cb7a0d14
Levgenix <mr.lobach@gmail.com>
2021-06-22 23:56:09 +0300

    Code style fixed

--- web/src/test/java/com/vet24/web/config/TestConfig.java
+++ web/src/test/java/com/vet24/web/config/TestConfig.java
@@ -3,7 +3,11 @@ package com.vet24.web.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
 import org.springframework.test.web.client.MockMvcClientHttpRequestFactory;
 import org.springframework.test.web.servlet.MockMvc;
 
