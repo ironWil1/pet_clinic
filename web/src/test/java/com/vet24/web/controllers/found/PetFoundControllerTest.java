57e11d92524d073cadcb47e73d3512b1f71fc8a4
Levgenix <mr.lobach@gmail.com>
2021-06-23 04:11:51 +0300

    PetFoundControllerTest ready for @WithUserDetails

--- web/src/test/java/com/vet24/web/controllers/found/PetFoundControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/found/PetFoundControllerTest.java
@@ -1,60 +1,51 @@
 package com.vet24.web.controllers.found;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
-import com.vet24.models.dto.pet.PetContactDto;
 import com.vet24.models.dto.pet.PetFoundDto;
 import com.vet24.models.pet.PetContact;
-import com.vet24.models.pet.PetFound;
 import com.vet24.service.pet.PetContactService;
 import com.vet24.web.ControllerAbstractIntegrationTest;
-import org.junit.FixMethodOrder;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.nio.charset.Charset;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @DBRider
+@WithUserDetails(value = "client1@email.com")
 public class PetFoundControllerTest extends ControllerAbstractIntegrationTest {
+
+    private final String URI = "http://localhost:8090/api/petFound";
 
     @Autowired
     PetContactService petContactService;
 
-    final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
     // get save data found pet and create with send owner message about pet - success
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/pet-found.yml", "/datasets/pet-contact.yml", "/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
-    public void testSaveDataFoundPetAndSendOwnerPetMessage() throws Exception {
+    public void testSaveDataFoundPetAndSendOwnerPetMessage() {
         PetContact petContact = petContactService.getByKey(104L);
-        String petCode = petContact.getPetCode();
-        final String URL_GET_PET_CONTACT_BY_PETCODE = "/api/petFound";
         PetFoundDto petFoundDto = new PetFoundDto("1.2345678", "2.3456789", "Some text");
-        String bodyUpdate = (new ObjectMapper()).valueToTree(petFoundDto).toString();
-        this.mockMvc.perform(MockMvcRequestBuilders.get(URL_GET_PET_CONTACT_BY_PETCODE)
-                .content(bodyUpdate).contentType(APPLICATION_JSON_UTF8)
-                .param("petCode", petCode))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andDo(MockMvcResultHandlers.print());
+        UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(URI)
+                .queryParam("petCode", petContact.getPetCode())
+                .build().encode();
+        ResponseEntity<PetFoundDto> response = testRestTemplate.postForEntity(uriComponents.toUri(), petFoundDto, PetFoundDto.class);
+        Assert.assertEquals(HttpStatus.CREATED, response.getStatusCode());
     }
 
     // get PetContact by petCode is not found - error 404
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/pet-found.yml", "/datasets/pet-contact.yml", "/datasets/user-entities.yml", "/datasets/pet-entities.yml"})
-    public void testSaveDataFoundPetAndSendOwnerPetMessageError404Pet() throws Exception {
-        String petCode = "CD0964F7A769B65E2BA57822840B0E53";
-        final String URL_GET_PET_CONTACT_BY_PETCODE = "/api/petFound";
+    public void testSaveDataFoundPetAndSendOwnerPetMessageError404Pet() {
         PetFoundDto petFoundDto = new PetFoundDto("1.2345678", "2.3456789", "Some text");
-        String bodyUpdate = (new ObjectMapper()).valueToTree(petFoundDto).toString();
-        this.mockMvc.perform(MockMvcRequestBuilders.get(URL_GET_PET_CONTACT_BY_PETCODE)
-                .content(bodyUpdate).contentType(APPLICATION_JSON_UTF8)
-                .param("petCode", petCode))
-                .andExpect(MockMvcResultMatchers.status().isNotFound())
-                .andDo(MockMvcResultHandlers.print());
+        UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(URI)
+                .queryParam("petCode", "CD0964F7A769B65E2BA57822840B0E53")
+                .build().encode();
+        ResponseEntity<PetFoundDto> response = testRestTemplate.postForEntity(uriComponents.toUri(), petFoundDto, PetFoundDto.class);
+        Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 }
