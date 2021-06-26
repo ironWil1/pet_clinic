06685584ec3e504ce1417f8a5119cf9288f61223
moms-array <khalyavinartem@yandex.ru>
2021-06-26 19:18:42 +0300

    - создал запросы для методов:  1) получение списка топиков по id клиента  2) получение топика под id клиента и заголовку топика

--- dao-impl/src/main/java/com/vet24/dao/user/TopicDaoImpl.java
+++ dao-impl/src/main/java/com/vet24/dao/user/TopicDaoImpl.java
@@ -4,6 +4,28 @@ import com.vet24.dao.ReadWriteDaoImpl;
 import com.vet24.models.user.Topic;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NoResultException;
+import java.util.List;
+
 @Repository
 public class TopicDaoImpl extends ReadWriteDaoImpl<Long, Topic> implements TopicDao {
+
+    @Override
+    public List<Topic> getTopicByClientId(Long id) {
+        return manager.createQuery("SELECT topic FROM Topic topic WHERE topic.topicStarter.id = :id", Topic.class)
+                .setParameter("id", id)
+                .getResultList();
+    }
+
+    @Override
+    public Topic getByTitleAndClientId(String title, Long clientId) {
+        try {
+            return manager.createQuery("SELECT topic FROM Topic topic where topic.title = :title AND topic.topicStarter.id = :clientId", Topic.class)
+                    .setParameter("title", title)
+                    .setParameter("clientId", clientId)
+                    .getSingleResult();
+        } catch (NoResultException e){
+            return null;
+        }
+    }
 }
