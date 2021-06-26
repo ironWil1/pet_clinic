e81676c3abc4e2e5254b423d50a5265f77713519
moms-array <khalyavinartem@yandex.ru>
2021-06-26 19:17:14 +0300

    - добавил метод поиска списка топиков по id клиента

--- dao-abstract/src/main/java/com/vet24/dao/user/TopicDao.java
+++ dao-abstract/src/main/java/com/vet24/dao/user/TopicDao.java
@@ -3,5 +3,11 @@ package com.vet24.dao.user;
 import com.vet24.dao.ReadWriteDao;
 import com.vet24.models.user.Topic;
 
+import java.util.List;
+
 public interface TopicDao extends ReadWriteDao<Long, Topic> {
+
+    List<Topic> getTopicByClientId(Long id);
+
+    Topic getByTitleAndClientId(String title, Long clientId);
 }
