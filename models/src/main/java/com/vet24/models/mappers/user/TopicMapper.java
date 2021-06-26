3b16288e937374e65d15e7e765fcf92f56726e6a
moms-array <khalyavinartem@yandex.ru>
2021-06-26 19:16:27 +0300

    - first commit

---
+++ models/src/main/java/com/vet24/models/mappers/user/TopicMapper.java
@@ -0,0 +1,16 @@
+package com.vet24.models.mappers.user;
+
+import com.vet24.models.dto.user.TopicDto;
+import com.vet24.models.mappers.DtoMapper;
+import com.vet24.models.mappers.EntityMapper;
+import com.vet24.models.user.Topic;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {CommentMapper.class})
+public interface TopicMapper extends DtoMapper <Topic, TopicDto>, EntityMapper <TopicDto, Topic> {
+
+    @Override
+    @Mapping(source = "topicStarter.id", target = "topicStarterId")
+    TopicDto toDto(Topic topic);
+}
