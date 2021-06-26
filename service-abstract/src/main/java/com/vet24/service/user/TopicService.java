6958cc60a2f542fce9666e5787ec1ee21c6d6327
moms-array <khalyavinartem@yandex.ru>
2021-06-26 19:18:59 +0300

    - создал 2 метода

--- service-abstract/src/main/java/com/vet24/service/user/TopicService.java
+++ service-abstract/src/main/java/com/vet24/service/user/TopicService.java
@@ -3,5 +3,13 @@ package com.vet24.service.user;
 import com.vet24.models.user.Topic;
 import com.vet24.service.ReadWriteService;
 
+import java.util.List;
+
 public interface TopicService extends ReadWriteService<Long, Topic> {
+
+    List<Topic> getTopicByClientId(Long id);
+
+    Topic getByTitleAndClientId(String title, Long clientId);
+
+
 }
