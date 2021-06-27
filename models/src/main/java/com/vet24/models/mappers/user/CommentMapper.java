ea7196933266dce7c8939f77b6bae3b2e1dee9cc
moms-array <khalyavinartem@yandex.ru>
2021-06-27 23:18:48 +0300

    - переопределил метод toDto

--- models/src/main/java/com/vet24/models/mappers/user/CommentMapper.java
+++ models/src/main/java/com/vet24/models/mappers/user/CommentMapper.java
@@ -5,7 +5,12 @@ import com.vet24.models.mappers.DtoMapper;
 import com.vet24.models.mappers.EntityMapper;
 import com.vet24.models.user.Comment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper extends DtoMapper<Comment, CommentDto>, EntityMapper<CommentDto, Comment> {
+
+    @Override
+    @Mapping(source = "user.id", target = "userId")
+    CommentDto toDto(Comment comment);
 }
