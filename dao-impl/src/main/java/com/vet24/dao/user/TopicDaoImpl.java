7f55c3ae97495512dbd3e2c083d4d6da1bf8016b
moms-array <khalyavinartem@yandex.ru>
2021-06-28 21:12:44 +0300

    - удалил лишний метод

--- dao-impl/src/main/java/com/vet24/dao/user/TopicDaoImpl.java
+++ dao-impl/src/main/java/com/vet24/dao/user/TopicDaoImpl.java
@@ -4,7 +4,6 @@ import com.vet24.dao.ReadWriteDaoImpl;
 import com.vet24.models.user.Topic;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.NoResultException;
 import java.util.List;
 
 @Repository
@@ -15,17 +14,5 @@ public class TopicDaoImpl extends ReadWriteDaoImpl<Long, Topic> implements Topic
         return manager.createQuery("SELECT topic FROM Topic topic WHERE topic.topicStarter.id = :id", Topic.class)
                 .setParameter("id", id)
                 .getResultList();
-    }
-
-    @Override
-    public Topic getByTitleAndClientId(String title, Long clientId) {
-        try {
-            return manager.createQuery("SELECT topic FROM Topic topic where topic.title = :title AND topic.topicStarter.id = :clientId", Topic.class)
-                    .setParameter("title", title)
-                    .setParameter("clientId", clientId)
-                    .getSingleResult();
-        } catch (NoResultException e){
-            return null;
-        }
     }
 }
