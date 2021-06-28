769e926d629bcdf7c679b311e59619443742f632
moms-array <khalyavinartem@yandex.ru>
2021-06-28 21:14:36 +0300

    - удалил лишний Mapping - добавил переопределнный метод toEntity

--- models/src/main/java/com/vet24/models/mappers/user/TopicMapper.java
+++ models/src/main/java/com/vet24/models/mappers/user/TopicMapper.java
@@ -7,11 +7,14 @@ import com.vet24.models.user.Topic;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {CommentMapper.class})
+@Mapper(componentModel = "spring", uses = {CommentMapper.class ,UserInfoMapper.class})
 public interface TopicMapper extends DtoMapper <Topic, TopicDto>, EntityMapper <TopicDto, Topic> {
 
     @Override
-    @Mapping(source = "topicStarter.id", target = "topicStarterId")
     @Mapping(source = "comments", target = "commentDtoList")
     TopicDto toDto(Topic topic);
+
+    @Override
+    @Mapping(source = "commentDtoList", target = "comments")
+    Topic toEntity (TopicDto topicDto);
 }
