192d94d24b2b2e683a3bc5bdb8a48d89f9b397d2
moms-array <khalyavinartem@yandex.ru>
2021-06-28 21:20:49 +0300

    - first commit

---
+++ models/src/main/java/com/vet24/models/dto/user/UserInfoDto.java
@@ -0,0 +1,17 @@
+package com.vet24.models.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoDto {
+
+    private Long id;
+    private String email;
+    private String firstname;
+    private String lastname;
+
+}
