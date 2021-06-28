4877b624a943259b910f5abefe3d4936d208f766
moms-array <khalyavinartem@yandex.ru>
2021-06-28 21:20:57 +0300

    - first commit

---
+++ models/src/main/java/com/vet24/models/mappers/user/UserInfoMapper.java
@@ -0,0 +1,12 @@
+package com.vet24.models.mappers.user;
+
+import com.vet24.models.dto.user.UserInfoDto;
+import com.vet24.models.mappers.DtoMapper;
+import com.vet24.models.mappers.EntityMapper;
+import com.vet24.models.user.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserInfoMapper extends DtoMapper<User, UserInfoDto>, EntityMapper<UserInfoDto, User> {
+
+}
