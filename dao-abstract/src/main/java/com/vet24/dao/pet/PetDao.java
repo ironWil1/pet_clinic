be2d7a95da4767d2109d188581fabd3f797d34f8
Sergey Smurov <sergey@mac.local>
2021-07-05 21:56:27 +0300

    dell sql querry into pet dao/impl

--- dao-abstract/src/main/java/com/vet24/dao/pet/PetDao.java
+++ dao-abstract/src/main/java/com/vet24/dao/pet/PetDao.java
@@ -2,10 +2,7 @@ package com.vet24.dao.pet;
 
 import com.vet24.dao.ReadWriteDao;
 import com.vet24.models.pet.Pet;
-import org.springframework.data.jpa.repository.Query;
 
 public interface PetDao extends ReadWriteDao<Long, Pet> {
 
-    @Query("select p from Pet p where p.id =:petId")
-    Long getPetId(Long petId);
 }
--- dao-impl/src/main/java/com/vet24/dao/pet/PetDaoImpl.java
+++ dao-impl/src/main/java/com/vet24/dao/pet/PetDaoImpl.java
@@ -4,13 +4,7 @@ import com.vet24.dao.ReadWriteDaoImpl;
 import com.vet24.models.pet.Pet;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.NoResultException;
-
 @Repository
 public class PetDaoImpl extends ReadWriteDaoImpl<Long, Pet> implements PetDao {
 
-    @Override
-    public Long getPetId(Long petId) {
-        return petId;
-    }
 }
