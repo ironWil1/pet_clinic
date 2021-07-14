897c87a650bd742f6d36be0b81907ad437546e94
yuri2711 <39201286+yuri2711@users.noreply.github.com>
2021-07-14 14:12:55 +0300

    Logger reload2
    Написал на проверку. Правильно ли  я сделал?

--- web/src/main/java/com/vet24/web/exceptionhandler/FileExceptionHandler.java
+++ web/src/main/java/com/vet24/web/exceptionhandler/FileExceptionHandler.java
@@ -2,14 +2,17 @@ package com.vet24.web.exceptionhandler;
 
 import com.vet24.models.dto.exception.ExceptionDto;
 import com.vet24.models.exception.StorageException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.FileNotFoundException;
 
 @ControllerAdvice
+@Slf4j
 public class FileExceptionHandler {
 
     @ExceptionHandler(FileNotFoundException.class)
@@ -17,14 +20,16 @@ public class FileExceptionHandler {
         ExceptionDto data = new ExceptionDto();
         data.setMessage(exception.getMessage());
 
+        log.info("File not found");
         return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(StorageException.class)
-    public ResponseEntity<ExceptionDto> handleException(StorageException exception) {
+    public ResponseEntity<ExceptionDto> handleException(StorageException exception, HttpServletRequest request) {
         ExceptionDto data = new ExceptionDto();
         data.setMessage(exception.getMessage());
-
+        log.info(request.getRequestURI());
+        log.info("Storage not found");
         return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
     }
 }
