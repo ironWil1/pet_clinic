5bacca8387403043018d9b195b8d1dea051b3209
DmitriFa <lipsuke@gmail.com>
2021-08-10 15:09:27 +0300

    10.08.21 devops

--- vaadin/src/main/java/com/vet24/vaadin/models/enums/PetSize.java
+++
@@ -1,7 +0,0 @@
-package com.vet24.vaadin.models.enums;
-
-public enum PetSize {
-    SMALL,
-    MEDIUM,
-    BIG
-}
--- vaadin/src/main/java/com/vet24/vaadin/models/enums/RoleNameEnum.java
+++
@@ -1,8 +0,0 @@
-package com.vet24.vaadin.models.enums;
-
-public enum RoleNameEnum {
-    ADMIN,
-    MANAGER,
-    CLIENT,
-    UNVERIFIED_CLIENT
-}
