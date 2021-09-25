a24eb6e597b357eb48cceb0ef48c85a7678ad843
Dmitriy Gusikov <cloud-1997@rambler.ru>
2021-09-25 22:13:54 +0300

    1.01 Tests for AdminCommentController.
    Changelog:
    1. Added check for tests
    2. Added 2 tests
    3. Removed admins.yml
    4. Added doctor-review.yml
    5. Replaced commentDao with commentService

--- web/src/test/java/com/vet24/web/user/AdminCommentControllerTest.java
+++ web/src/test/java/com/vet24/web/user/AdminCommentControllerTest.java
@@ -2,32 +2,44 @@ package com.vet24.web.user;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.vet24.models.dto.user.CommentDto;
+import com.vet24.models.dto.user.UserInfoDto;
+import com.vet24.service.user.CommentService;
 import com.vet24.web.ControllerAbstractIntegrationTest;
-import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.util.NestedServletException;
 
-@WithUserDetails(value = "user3@gmail.com")
-@Slf4j
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.time.LocalDateTime;
+
 public class AdminCommentControllerTest extends ControllerAbstractIntegrationTest {
 
     private final String URI = "http://localhost:8080/api/admin/comment";
     private CommentDto commentDto;
-//    private CommentService commentService;
-//
-//    @Autowired
-//    public AdminCommentControllerTest(CommentService commentService) {
-//        this.commentService = commentService;
-//    }
+    @Autowired
+    private CommentService commentService;
+
+    @Before
+    public void createCommentDto() {
+        commentDto = new CommentDto();
+        commentDto.setId(101L);
+        commentDto.setUserInfoDto(new UserInfoDto());
+        commentDto.setContent("testComment");
+        commentDto.setDateTime(LocalDateTime.now());
+        commentDto.setLikes(1);
+        commentDto.setDislike(0);
+    }
 
     @Test
-    @DataSet(cleanBefore = true, value = {"/datasets/admins.yml", "/datasets/comments.yml", "/datasets/clients.yml"})
+    @DataSet(cleanBefore = true, value = {"/datasets/comments.yml", "/datasets/clients.yml"})
     public void commentUpdatedNotFound() throws Exception {
-        commentDto = new CommentDto();
-        commentDto.setContent("testComment");
+        commentDto.setContent("updatedTestComment");
         mockMvc.perform(MockMvcRequestBuilders.put(URI + "/{id}", 1_000_000)
                         .content(objectMapper.valueToTree(commentDto).toString())
                         .contentType(MediaType.APPLICATION_JSON))
@@ -35,22 +47,39 @@ public class AdminCommentControllerTest extends ControllerAbstractIntegrationTes
     }
 
     @Test
-    @DataSet(cleanBefore = true, value = {"/datasets/admins.yml", "/datasets/comments.yml", "/datasets/clients.yml"})
-    public void commentUpdated() throws Exception {
-        commentDto = new CommentDto();
-        commentDto.setContent("testComment");
-        mockMvc.perform(MockMvcRequestBuilders.put(URI + "/{id}", 101)
-                        .content(objectMapper.valueToTree(commentDto).toString())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+    @DataSet(cleanBefore = true, value = {"/datasets/comments.yml", "/datasets/clients.yml"})
+    public void commentDeletedNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete(URI + "/{id}", 1_000_000))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
     @Test
-    @DataSet(cleanBefore = true, value = {"/datasets/admins.yml", "/datasets/comments.yml", "/datasets/clients.yml"})
-    public void commentDeleted() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete(URI + "/{id}", 101))
+    @DataSet(cleanBefore = true, value = {"/datasets/comments.yml", "/datasets/clients.yml"})
+    public void commentUpdated() throws Exception {
+        String tempComment = commentDto.getContent();
+        commentDto.setContent("updatedTestComment");
+        mockMvc.perform(MockMvcRequestBuilders.put(URI + "/{id}", 103)
+                        .content(objectMapper.valueToTree(commentDto).toString())
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
+        assertThat(tempComment).isNotEqualTo(commentDto.getContent());
     }
 
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/comments.yml", "/datasets/clients.yml"})
+    public void commentDeleted() throws Exception {
+        int sizeBefore = commentService.getAll().size();
+        mockMvc.perform(MockMvcRequestBuilders.delete(URI + "/{id}", 101))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        assertThat(--sizeBefore).isEqualTo(commentService.getAll().size());
+    }
 
+    @Test(expected = NestedServletException.class)
+    @DataSet(cleanBefore = true, value = {"/datasets/comments.yml", "/datasets/clients.yml",
+            "/datasets/doctors.yml", "/datasets/doctor-review.yml"})
+    public void commentNotDeletedBecauseConstraints() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete(URI + "/{id}", 102))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
+    }
 }
