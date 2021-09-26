4142bf4ead88913bd5b0afaa281c484417ace02b
Gorkadobr <gorkadobrrr@yandex.ru>
2021-09-26 20:59:23 +0300

    1. В ClientReviewControllerTest-тестах на update и delete добавлены проверки.

--- web/src/test/java/com/vet24/web/controllers/user/ClientReviewControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/user/ClientReviewControllerTest.java
@@ -1,10 +1,10 @@
 package com.vet24.web.controllers.user;
 
 import com.github.database.rider.core.api.dataset.DataSet;
-import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.vet24.dao.user.CommentDao;
 import com.vet24.models.user.Client;
 import com.vet24.service.user.ClientService;
+import com.vet24.service.user.DoctorReviewService;
 import com.vet24.web.ControllerAbstractIntegrationTest;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,6 +14,7 @@ import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 @WithUserDetails(value = "user3@gmail.com")
@@ -23,6 +24,13 @@ public class ClientReviewControllerTest extends ControllerAbstractIntegrationTes
 
     @Autowired
     private ClientService clientService;
+
+    @Autowired
+    private CommentDao commentDao;
+
+    @Autowired
+    private DoctorReviewService doctorReviewService;
+
 
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/clients.yml", "/datasets/doctors.yml", "/datasets/comments.yml"})
@@ -68,6 +76,7 @@ public class ClientReviewControllerTest extends ControllerAbstractIntegrationTes
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
+        assertThat(doctorReviewService.getByDoctorAndClientId(33,3).getComment().getContent()).isEqualTo("This doctor is very bad!!");
     }
 
     @Test
@@ -83,17 +92,21 @@ public class ClientReviewControllerTest extends ControllerAbstractIntegrationTes
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/clients.yml", "/datasets/comments.yml", "/datasets/doctor-review.yml", "/datasets/doctors.yml"})
     public void shouldBeDeleteComment() throws Exception {
+        int beforeCount = commentDao.getAll().size();
         mockMvc.perform(MockMvcRequestBuilders.delete(URI + "/{doctorId}/review", 33))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
+        assertThat(--beforeCount).isEqualTo(commentDao.getAll().size());
     }
 
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/clients.yml", "/datasets/comments.yml","/datasets/doctor-review.yml", "/datasets/doctors.yml"})
     public void deletedCommentNotFound() throws Exception {
+        int beforeCount = commentDao.getAll().size();
         mockMvc.perform(MockMvcRequestBuilders.delete(URI + "/{doctorId}/review", 50000))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().is4xxClientError());
+        assertThat(beforeCount).isEqualTo(commentDao.getAll().size());
     }
 
 
