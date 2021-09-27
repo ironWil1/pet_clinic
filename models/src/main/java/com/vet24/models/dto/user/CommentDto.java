c4e85790fac292cdb6541a7261e40594086f7ea6
KARIM-Shakh <karim.waxabai@mail.ru>
2021-09-27 16:54:13 +0600

    "fixed CommentDto"

--- models/src/main/java/com/vet24/models/dto/user/CommentDto.java
+++ models/src/main/java/com/vet24/models/dto/user/CommentDto.java
@@ -3,18 +3,14 @@ package com.vet24.models.dto.user;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
 public class CommentDto {
 
     private Long id;
-    @NotNull
     private UserInfoDto UserInfoDto;
     @NotBlank
-    @Size(min = 15)
     private String content;
     private LocalDateTime dateTime;
     private int likes;
