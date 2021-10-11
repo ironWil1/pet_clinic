7634078dd3e6f28f6d63e0d16ea6a26a1e6a6e7c
Alexander Shcherbakov <zuigetsu@gmail.com>
2021-10-11 17:30:14 +0300

    fix cors

--- security/src/main/java/com/vet24/security/config/SecurityConfig.java
+++ security/src/main/java/com/vet24/security/config/SecurityConfig.java
@@ -102,6 +102,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
