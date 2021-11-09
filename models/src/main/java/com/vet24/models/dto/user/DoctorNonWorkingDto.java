61536a5ec17c99ab31bf47131ae0a0fd6695f62f
Andrey Skityba <kk@mm.st>
2021-11-09 21:29:25 +0300

    add doctorNonWorkingId

--- models/src/main/java/com/vet24/models/dto/user/DoctorNonWorkingDto.java
+++ models/src/main/java/com/vet24/models/dto/user/DoctorNonWorkingDto.java
@@ -4,14 +4,13 @@ import com.vet24.models.enums.DayOffType;
 import com.vet24.models.user.Doctor;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Data
 public class DoctorNonWorkingDto {
 
-    private Long doctorId;
+    private Long Id;
+    private Doctor doctor;
     private DayOffType type;
     private LocalDate date;
-
 }
