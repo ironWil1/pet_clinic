8d22040c4e354fc47653b1bc829cdefdb0215d8b
Andrey Skityba <kk@mm.st>
2021-11-09 23:04:05 +0300

    Revert "add doctorNonWorkingId"
    This reverts commit 61536a5ec17c99ab31bf47131ae0a0fd6695f62f.

--- models/src/main/java/com/vet24/models/dto/user/DoctorNonWorkingDto.java
+++ models/src/main/java/com/vet24/models/dto/user/DoctorNonWorkingDto.java
@@ -4,13 +4,14 @@ import com.vet24.models.enums.DayOffType;
 import com.vet24.models.user.Doctor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Data
 public class DoctorNonWorkingDto {
 
-    private Long Id;
-    private Doctor doctor;
+    private Long doctorId;
     private DayOffType type;
     private LocalDate date;
+
 }
