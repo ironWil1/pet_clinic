1dd2c3dbdf21a1688d0a92fa00d3db9e88db0198
Valentin-java <v_tin@mail.ru>
2021-11-21 15:33:33 +0300

    authenticateUser controller refactoring v1.5

--- web/src/main/java/com/vet24/web/controllers/user/AuthController.java
+++ web/src/main/java/com/vet24/web/controllers/user/AuthController.java
@@ -3,6 +3,7 @@ package com.vet24.web.controllers.user;
 import com.vet24.models.exception.BadRequestException;
 import com.vet24.security.config.JwtUtils;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,15 +25,16 @@ public class AuthController {
     @Autowired
     JwtUtils jwtUtils;
 
-
-    @ApiResponse(responseCode = "200", description = "Token was succesful generated")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Token was succesful generated"),
+            @ApiResponse(responseCode = "400", description = "Something went wrong")
+    })
     @PostMapping("/auth")
     public ResponseEntity<String> authenticateUser(@RequestBody AuthRequest authRequest) {
-        if(authRequest == null) {
+        if (authRequest == null) {
             throw new BadRequestException(INVALID_TOKEN_MSG);
         }
         return new ResponseEntity<>(jwtUtils.generateJwtToken(authRequest.getUsername()), HttpStatus.OK);
-
     }
 
 }
