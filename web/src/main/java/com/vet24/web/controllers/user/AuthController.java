ecb482714c2e30a6ef3aba7055b4a9bcb76f4a2f
Valentin-java <v_tin@mail.ru>
2021-11-21 15:26:25 +0300

    authenticateUser controller refactoring v1.4

--- web/src/main/java/com/vet24/web/controllers/user/AuthController.java
+++ web/src/main/java/com/vet24/web/controllers/user/AuthController.java
@@ -2,6 +2,8 @@ package com.vet24.web.controllers.user;
 
 import com.vet24.models.exception.BadRequestException;
 import com.vet24.security.config.JwtUtils;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Tag(name = "Auth Controller", description = "response token")
 public class AuthController {
     private static final String INVALID_TOKEN_MSG = "Registration token is invalid";
 
@@ -22,6 +25,7 @@ public class AuthController {
     JwtUtils jwtUtils;
 
 
+    @ApiResponse(responseCode = "200", description = "Token was succesful generated")
     @PostMapping("/auth")
     public ResponseEntity<String> authenticateUser(@RequestBody AuthRequest authRequest) {
         if(authRequest == null) {
