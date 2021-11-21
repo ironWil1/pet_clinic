ee6ad8e3f857b0cb5b613d911bbb15a1cbd0981e
Valentin-java <v_tin@mail.ru>
2021-11-21 15:17:43 +0300

    authenticateUser controller refactoring v1.3

--- web/src/main/java/com/vet24/web/controllers/user/AuthController.java
+++ web/src/main/java/com/vet24/web/controllers/user/AuthController.java
@@ -1,12 +1,11 @@
 package com.vet24.web.controllers.user;
 
+import com.vet24.models.exception.BadRequestException;
 import com.vet24.security.config.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    private static final String INVALID_TOKEN_MSG = "Registration token is invalid";
+
     @Autowired
     AuthenticationManager authenticationManager;
 
@@ -23,11 +24,11 @@ public class AuthController {
 
     @PostMapping("/auth")
     public ResponseEntity<String> authenticateUser(@RequestBody AuthRequest authRequest) {
-        if (!authRequest.getUsername().contains("")) {
-        return new ResponseEntity<>(jwtUtils.generateJwtToken(authRequest.getUsername()), HttpStatus.OK);
-        } else {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if(authRequest == null) {
+            throw new BadRequestException(INVALID_TOKEN_MSG);
         }
+        return new ResponseEntity<>(jwtUtils.generateJwtToken(authRequest.getUsername()), HttpStatus.OK);
+
     }
 
 }
