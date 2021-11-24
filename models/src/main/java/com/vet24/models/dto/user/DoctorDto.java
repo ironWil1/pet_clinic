9d53ffcc28aa2b9c44fb7f89acdd8a46456c4758
Alexander Shcherbakov <zuigetsu@gmail.com>
2021-11-24 15:29:03 +0300

    fix imports

--- models/src/main/java/com/vet24/models/dto/user/DoctorDto.java
+++ models/src/main/java/com/vet24/models/dto/user/DoctorDto.java
@@ -1,10 +1,7 @@
 package com.vet24.models.dto.user;
 
-import com.vet24.models.dto.medicine.DiagnosisDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 @AllArgsConstructor
--- web/src/main/java/com/vet24/web/exceptionhandler/DoctorNonWorkingExceptionHandler.java
+++ web/src/main/java/com/vet24/web/exceptionhandler/DoctorNonWorkingExceptionHandler.java
@@ -2,7 +2,6 @@ package com.vet24.web.exceptionhandler;
 
 import com.vet24.models.dto.exception.ExceptionDto;
 import com.vet24.models.exception.DoctorNonWorkingEventException;
-import com.vet24.models.exception.RepeatedCommentException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
