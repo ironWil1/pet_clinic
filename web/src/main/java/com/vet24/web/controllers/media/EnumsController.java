29e23f3f9f23f7701e17b94ea2040a37f86c84f8
Valentin-java <v_tin@mail.ru>
2021-11-29 17:57:39 +0300

    147-EnumsController v1.1

--- web/src/main/java/com/vet24/web/controllers/media/EnumsController.java
+++ web/src/main/java/com/vet24/web/controllers/media/EnumsController.java
@@ -1,5 +1,7 @@
 package com.vet24.web.controllers.media;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.reflections.Reflections;
 import org.springframework.http.HttpStatus;
@@ -9,15 +11,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 @RestController
-@Tag(name = "Enums Controller", description = "Return lists")
+@Tag(name = "Enums Controller", description = "Return all Enum's n return all constant's of the \"nameEnum\"")
 public class EnumsController {
 
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return all of the Enum's"),
+            @ApiResponse(responseCode = "40x", description = "Something went wrong")
+    })
     @GetMapping("/api/enums")
     public ResponseEntity<List> findAllEnums() {
         List<String> result = new ArrayList<>();
@@ -29,8 +36,12 @@ public class EnumsController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return all constant's of the \"nameEnum\""),
+            @ApiResponse(responseCode = "40x", description = "Something went wrong")
+    })
     @GetMapping("/api/enums/{enumName}")
-    public ResponseEntity<List> getEnumNameList(@PathVariable String enumName) {
+    public ResponseEntity<List> getEnumNameList(@Valid @PathVariable String enumName) {
         String pkg = "com.vet24.models.enums";
         String found = "com.vet24.models.enums." + enumName;
         List<String> result = new ArrayList<>();
