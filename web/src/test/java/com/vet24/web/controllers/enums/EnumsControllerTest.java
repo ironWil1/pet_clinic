d1c41b2cb375a2d78aef5c5c8f44e48ef11af391
Valentin-java <v_tin@mail.ru>
2021-12-01 13:21:45 +0300

    147-EnumsController v1.6

--- web/src/test/java/com/vet24/web/controllers/enums/EnumsControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/enums/EnumsControllerTest.java
@@ -1,7 +1,48 @@
 package com.vet24.web.controllers.enums;
 
-import groovy.util.logging.Slf4j;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.vet24.web.ControllerAbstractIntegrationTest;
+import com.vet24.web.controllers.media.EnumsController;
+import com.vet24.web.util.ReflectionUtil;
+import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@Slf4j
-public class EnumsControllerTest {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WithUserDetails("admin@gmail.com")
+class EnumsControllerTest extends ControllerAbstractIntegrationTest {
+
+    @Autowired
+    private ReflectionUtil reflectionUtil;
+
+    @Autowired
+    private EnumsController enumsController;
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml"})
+    public void findAllEnums() {
+        assertThat(reflectionUtil.getAllEnums()).isEqualTo(enumsController.findAllEnums().getBody());
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml"})
+    public void checkConstantList() {
+        List<String> genList = reflectionUtil.getAllEnums();
+        for (String name : genList) {
+            assertThat((reflectionUtil.getEnumConsts(name))).isEqualTo(enumsController.getEnumNameList(name).getBody());
+        }
+    }
+
 }
