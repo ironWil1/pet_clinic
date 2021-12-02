88d177ea08185c48b0bf07361eeb9a9a90687f9f
Valentin-java <v_tin@mail.ru>
2021-12-02 22:28:57 +0300

    147-EnumsController v1.6.4

--- web/src/test/java/com/vet24/web/controllers/enums/EnumsControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/enums/EnumsControllerTest.java
@@ -1,14 +1,29 @@
 package com.vet24.web.controllers.enums;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.vet24.models.dto.pet.AbstractNewPetDto;
 import com.vet24.web.ControllerAbstractIntegrationTest;
-import com.vet24.web.controllers.media.EnumsController;
 import com.vet24.web.util.ReflectionUtil;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
 import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
 
 @WithUserDetails("client1@email.com")
 public class EnumsControllerTest extends ControllerAbstractIntegrationTest {
@@ -18,24 +33,30 @@ public class EnumsControllerTest extends ControllerAbstractIntegrationTest {
     @Autowired
     private ReflectionUtil reflectionUtil;
 
-    @Autowired
-    private EnumsController enumsController;
 
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml"})
-    public void findAllEnums() {
+    public void findAllEnums() throws Exception {
 
-
-        assertThat(reflectionUtil.getAllEnums()).isEqualTo(enumsController.findAllEnums().getBody());
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(URI)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+        List<String> list = objectMapper.readValue(
+                result.getResponse().getContentAsString(),
+                new TypeReference<List<String>>() {});
+        assertThat(list).isEqualTo(reflectionUtil.getAllEnums());
     }
 
-    @Test
+
+   /* @Test
     @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml"})
     public void checkConstantList() throws ClassNotFoundException {
         List<String> genList = reflectionUtil.getAllEnums();
         for (String name : genList) {
-            assertThat((reflectionUtil.getEnumConsts(name))).isEqualTo(enumsController.getEnumNameList(name).getBody());
+
+            //assertThat(enumsController.getEnumNameList(name).getBody()).isEqualTo((reflectionUtil.getEnumConsts(name)));
         }
-    }
+    }*/
 
 }
