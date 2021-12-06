ccac7306a0ed7b90c8a3a8b7680dcc85f9dba48a
Alexander Shcherbakov <zuigetsu@gmail.com>
2021-12-06 13:03:01 +0300

    fix

--- web/src/main/java/com/vet24/web/controllers/media/EnumsController.java
+++ web/src/main/java/com/vet24/web/controllers/media/EnumsController.java
@@ -9,12 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/enums")
 @Tag(name = "Enums Controller", description = "Return all Enum's n return all constant's of the \"nameEnum\"")
 public class EnumsController {
 
@@ -29,7 +31,7 @@ public class EnumsController {
             @ApiResponse(responseCode = "200", description = "Return all of the Enum's"),
             @ApiResponse(responseCode = "40x", description = "Something went wrong")
     })
-    @GetMapping("/api/enums")
+    @GetMapping
     public ResponseEntity<List<String>> findAllEnums() {
         return new ResponseEntity<>(reflectionUtil.getAllEnums(), HttpStatus.OK);
     }
@@ -38,7 +40,7 @@ public class EnumsController {
             @ApiResponse(responseCode = "200", description = "Return all constant's of the \"nameEnum\""),
             @ApiResponse(responseCode = "40x", description = "Something went wrong")
     })
-    @GetMapping("/api/enums/{enumName}")
+    @GetMapping("{enumName}")
     public ResponseEntity<List<String>> getEnumNameList(@Valid @PathVariable String enumName) {
         try {
         return new ResponseEntity<>(reflectionUtil.getEnumConsts(enumName), HttpStatus.OK);
