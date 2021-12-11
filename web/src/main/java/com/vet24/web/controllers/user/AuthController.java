20605d786b0b8c43753eefdb0db37af13121993b
minlebay <ilshatminnibaev@yandex.ru>
2021-12-11 15:01:29 +0300

    added notblank in authRequest

--- web/src/main/java/com/vet24/web/controllers/user/AuthController.java
+++ web/src/main/java/com/vet24/web/controllers/user/AuthController.java
@@ -14,8 +14,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import javax.validation.Valid;
 
+import javax.validation.Valid;
 
 @RestController
 @Tag(name = "Auth Controller", description = "response token")
--- web/src/main/java/com/vet24/web/controllers/user/AuthRequest.java
+++ web/src/main/java/com/vet24/web/controllers/user/AuthRequest.java
@@ -3,11 +3,14 @@ package com.vet24.web.controllers.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 public class AuthRequest implements Serializable {
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
 }
