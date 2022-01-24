f234efdcee5b6adc96ceaa4c2d573f8db90bb3f8
AlexKanivets <al.kanivets@mail.ru>
2022-01-24 19:16:17 +0300

    add dto+objectMapper

--- web/src/test/java/com/vet24/web/controllers/user/AuthControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/user/AuthControllerTest.java
@@ -1,12 +1,18 @@
 package com.vet24.web.controllers.user;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.vet24.models.dto.user.CommentDto;
+import com.vet24.models.dto.user.RegisterDto;
+import com.vet24.models.dto.user.UserInfoDto;
 import com.vet24.web.ControllerAbstractIntegrationTest;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
 
 public class AuthControllerTest extends ControllerAbstractIntegrationTest {
     @Autowired
@@ -17,9 +23,10 @@ public class AuthControllerTest extends ControllerAbstractIntegrationTest {
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml"})
     public void authAdmin() throws Exception {
+        AuthRequest authRequest = new AuthRequest("admin1@email.com", "admin");
         mockMvc.perform(MockMvcRequestBuilders.post(URI)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\": \"admin1@email.com\", \"password\": \"admin\"}"))
+                .content(objectMapper.valueToTree(authRequest).toString()))
                 .andExpect(MockMvcResultMatchers.jsonPath("jwtToken").isNotEmpty())
                 .andExpect(MockMvcResultMatchers.jsonPath("role").value("ADMIN"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
@@ -28,9 +35,10 @@ public class AuthControllerTest extends ControllerAbstractIntegrationTest {
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml"})
     public void authManager() throws Exception {
+        AuthRequest authRequest = new AuthRequest("manager1@email.com", "manager");
         mockMvc.perform(MockMvcRequestBuilders.post(URI)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\": \"manager1@email.com\", \"password\": \"manager\"}"))
+                .content(objectMapper.valueToTree(authRequest).toString()))
                 .andExpect(MockMvcResultMatchers.jsonPath("jwtToken").isNotEmpty())
                 .andExpect(MockMvcResultMatchers.jsonPath("role").value("MANAGER"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
@@ -39,9 +47,10 @@ public class AuthControllerTest extends ControllerAbstractIntegrationTest {
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml"})
     public void authDoctor() throws Exception {
+        AuthRequest authRequest = new AuthRequest("doctor103@email.com", "doctor");
         mockMvc.perform(MockMvcRequestBuilders.post(URI)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\": \"doctor103@email.com\", \"password\": \"doctor\"}"))
+                .content(objectMapper.valueToTree(authRequest).toString()))
                 .andExpect(MockMvcResultMatchers.jsonPath("jwtToken").isNotEmpty())
                 .andExpect(MockMvcResultMatchers.jsonPath("role").value("DOCTOR"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
@@ -50,9 +59,10 @@ public class AuthControllerTest extends ControllerAbstractIntegrationTest {
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml"})
     public void authClient() throws Exception {
+        AuthRequest authRequest = new AuthRequest("client1@email.com", "client");
         mockMvc.perform(MockMvcRequestBuilders.post(URI)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\": \"client1@email.com\", \"password\": \"client\"}"))
+                .content(objectMapper.valueToTree(authRequest).toString()))
                 .andExpect(MockMvcResultMatchers.jsonPath("jwtToken").isNotEmpty())
                 .andExpect(MockMvcResultMatchers.jsonPath("role").value("CLIENT"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
@@ -61,9 +71,10 @@ public class AuthControllerTest extends ControllerAbstractIntegrationTest {
     @Test
     @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml"})
     public void authBadLogin() throws Exception {
+        AuthRequest authRequest = new AuthRequest("test@email.com", "test");
         mockMvc.perform(MockMvcRequestBuilders.post(URI)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\": \"test@email.com\", \"password\": \"test\"}"))
+                .content(objectMapper.valueToTree(authRequest).toString()))
                 .andExpect(MockMvcResultMatchers.status().is(403));
     }
 }
