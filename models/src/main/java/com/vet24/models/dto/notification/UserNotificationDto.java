79039dfcc707986e5f37cb515c7da9d4c486260b
Alla <liwinchester2y5@gmail.com>
2022-03-17 15:12:23 +0300

    224 - добавила метод updateEntity

--- models/src/main/java/com/vet24/models/dto/notification/UserNotificationDto.java
+++ models/src/main/java/com/vet24/models/dto/notification/UserNotificationDto.java
@@ -3,9 +3,7 @@ package com.vet24.models.dto.notification;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
--- web/src/main/java/com/vet24/web/controllers/notification/UserNotificationController.java
+++ web/src/main/java/com/vet24/web/controllers/notification/UserNotificationController.java
@@ -9,7 +9,6 @@ import com.vet24.service.notification.UserNotificationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,20 +17,20 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
-@Slf4j
 @RestController
+@Slf4j
 @RequestMapping("/api/user/notification")
 public class UserNotificationController {
 
     private final UserNotificationService userNotificationService;
     private final UserNotificationMapper userNotificationMapper;
 
-    @Autowired
     public UserNotificationController(UserNotificationService userNotificationService,
                                       UserNotificationMapper userNotificationMapper) {
         this.userNotificationService = userNotificationService;
         this.userNotificationMapper = userNotificationMapper;
     }
+
     @Operation(summary = "Getting all notifications for the user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Notification list received"),
