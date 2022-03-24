e92615d4ab260c3ab7deeb5b299b47484e95cfe4
Alla <liwinchester2y5@gmail.com>
2022-03-24 15:22:09 +0300

    229- Тест для UserNotificationController

--- web/src/test/java/com/vet24/web/controllers/notification/UserNotificationControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/notification/UserNotificationControllerTest.java
@@ -4,7 +4,7 @@ import com.github.database.rider.core.api.dataset.DataSet;
 import com.vet24.models.dto.notification.UserNotificationDto;
 import com.vet24.models.notification.UserNotification;
 import com.vet24.web.ControllerAbstractIntegrationTest;
-import org.junit.Assert;
+import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +17,7 @@ import javax.persistence.EntityManager;
 
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 public class UserNotificationControllerTest extends ControllerAbstractIntegrationTest {
 
@@ -42,39 +42,40 @@ public class UserNotificationControllerTest extends ControllerAbstractIntegratio
     }
 
     @Test
-    @DataSet(cleanBefore = true, value = {"/datasets/user-entities.yml", "/datasets/notification/user_notification.yml",
-            "/datasets/notification/notification.yml"})
+    @DataSet(cleanBefore = true, value = {"/datasets/controllers/userNotification_controller/user-entities.yml",
+            "/datasets/controllers/userNotification_controller/user_notification.yml",
+            "/datasets/controllers/userNotification_controller/notification.yml"})
     public void testGetAllNotifications() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(URI)
                         .header("Authorization", "Bearer " + token)
                         .content(objectMapper.writeValueAsString(userNotificationDto))
                         .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().string("{\"id\":1,\"content\":\"User Notification Test 1\",\"important\":true}"));
-        assertThat(entityManager.createQuery("FROM UserNotification un " +
-                "WHERE un.user.id=100", UserNotification.class).getSingleResult().getNotification().getContent())
-                .isEqualTo("UserNotification Test 1");
+                .andExpect(jsonPath("$[0].id", Is.is(1)))
+                .andExpect(jsonPath("$[0].content", Is.is("User Notification Test 1")))
+                .andExpect(jsonPath("$[0].important", Is.is(true)));
     }
 
     @Test
-    @DataSet(value = {"/datasets/user-entities.yml", "/datasets/notification/user_notification.yml",
-            "/datasets/notification/notification.yml"}, cleanBefore = true)
+    @DataSet(value = {"/datasets/controllers/userNotification_controller/user-entities.yml",
+            "/datasets/controllers/userNotification_controller/user_notification.yml",
+            "/datasets/controllers/userNotification_controller/notification.yml"}, cleanBefore = true)
     public void testGetUserNotificationById() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(URI + "/{notificationId}", 5)
                         .header("Authorization", "Bearer " + token)
                         .content(objectMapper.writeValueAsString(userNotificationDto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().string("{\"id\":1,\"content\":\"User Notification Test 1\",\"important\":true}"));
-        assertThat(entityManager.createQuery("FROM UserNotification un " +
-                "WHERE un.id=5", UserNotification.class).getSingleResult().getNotification().getContent())
-                .isEqualTo("UserNotification Test 1");
+                .andExpect(jsonPath("$.id", Is.is(1)))
+                .andExpect(jsonPath("$.content", Is.is("User Notification Test 1")))
+                .andExpect(jsonPath("$.important", Is.is(true)));
 
     }
 
     @Test
-    @DataSet(value = {"/datasets/user-entities.yml", "/datasets/notification/user_notification.yml",
-            "/datasets/notification/notification.yml"}, cleanBefore = true)
+    @DataSet(value = {"/datasets/controllers/userNotification_controller/user-entities.yml",
+            "/datasets/controllers/userNotification_controller/user_notification.yml",
+            "/datasets/controllers/userNotification_controller/notification.yml"}, cleanBefore = true)
     public void testNotificationsStatus() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.put(URI + "/{notificationId}", 5)
                         .header("Authorization", "Bearer " + token)
