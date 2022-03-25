0dc01a1d875fe061de60a3215950fc339d344369
Alla <liwinchester2y5@gmail.com>
2022-03-25 22:46:23 +0300

    229- Тест для UserNotificationController

--- web/src/main/resources/db/migration/V3__DB_comment_remove_on_delete_cascade.sql
+++ web/src/main/resources/db/migration/V3__DB_comment_remove_on_delete_cascade.sql
@@ -1,5 +1,5 @@
--- alter table comment
--- drop constraint fk_comment_user_entities,
--- add constraint fk_comment_user_entities
---     foreign key (user_id)
---         references user_entities;
+alter table comment
+drop constraint fk_comment_user_entities,
+add constraint fk_comment_user_entities
+    foreign key (user_id)
+        references user_entities;
