2977f60e3ce9262b330493db5ade898e1ee8ae6d
Alexander Shcherbakov <zuigetsu@gmail.com>
2022-04-13 17:04:23 +0300

    fix

--- web/src/main/java/com/vet24/web/controllers/user/AdminDoctorController.java
+++ web/src/main/java/com/vet24/web/controllers/user/AdminDoctorController.java
@@ -3,21 +3,27 @@ package com.vet24.web.controllers.user;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.vet24.models.dto.user.DoctorDto;
 import com.vet24.models.dto.user.DoctorDtoPost;
-import com.vet24.models.mappers.user.DoctorPostMapper;
 import com.vet24.models.mappers.user.DoctorMapper;
+import com.vet24.models.mappers.user.DoctorPostMapper;
 import com.vet24.models.user.Doctor;
 import com.vet24.models.util.View;
-import com.vet24.service.user.DoctorServiceImpl;
+import com.vet24.service.user.DoctorService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.webjars.NotFoundException;
 
 import javax.validation.Valid;
@@ -29,20 +35,20 @@ import java.util.List;
 @Tag(name = "AdminDoctor controller", description = "CRUD operations")
 public class AdminDoctorController {
 
-    private final DoctorServiceImpl doctorServiceImpl;
+    private final DoctorService doctorService;
     private final DoctorMapper doctorMapper;
     private final DoctorPostMapper doctorPostMapper;
 
 
-    public AdminDoctorController(DoctorServiceImpl doctorServiceImpl, DoctorMapper doctorMapper, DoctorPostMapper doctorPostMapper) {
-        this.doctorServiceImpl = doctorServiceImpl;
+    public AdminDoctorController(DoctorService doctorServiceImpl, DoctorMapper doctorMapper, DoctorPostMapper doctorPostMapper) {
+        this.doctorService = doctorServiceImpl;
         this.doctorMapper = doctorMapper;
         this.doctorPostMapper = doctorPostMapper;
     }
 
     @GetMapping("{id}")
     public ResponseEntity<DoctorDto> getDoctorById(@PathVariable Long id) {
-        Doctor doctor = doctorServiceImpl.getByKey(id);
+        Doctor doctor = doctorService.getByKey(id);
         if (doctor != null) {
             DoctorDto doctorDto = doctorMapper.toDto(doctor);
             return ResponseEntity.ok(doctorDto);
@@ -54,7 +60,7 @@ public class AdminDoctorController {
 
     @GetMapping("")
     public ResponseEntity<List<DoctorDto>> getAllDoctors() {
-        List<Doctor> doctorList = doctorServiceImpl.getAll();
+        List<Doctor> doctorList = doctorService.getAll();
         return ResponseEntity.ok(doctorMapper.toDto(doctorList));
     }
 
@@ -69,7 +75,7 @@ public class AdminDoctorController {
     @PostMapping
     public ResponseEntity<DoctorDto> doctorDtoPost(@JsonView(View.Post.class) @Valid @RequestBody DoctorDtoPost doctorDtoPost) {
         Doctor doctor = doctorPostMapper.toEntity(doctorDtoPost);
-        doctorServiceImpl.persist(doctor);
+        doctorService.persist(doctor);
         return ResponseEntity.status(200).body(doctorPostMapper.toDto(doctor));
     }
 
@@ -83,12 +89,12 @@ public class AdminDoctorController {
     @PutMapping("{id}")
     public ResponseEntity<DoctorDto> doctorDtoPut(@JsonView(View.Put.class) @Valid @RequestBody DoctorDtoPost doctorDtoPost,
                                                   @PathVariable("id") long id) {
-        Doctor doctor = doctorServiceImpl.getByKey(id);
+        Doctor doctor = doctorService.getByKey(id);
         doctor.setFirstname(doctorDtoPost.getFirstname());
         doctor.setLastname(doctorDtoPost.getLastname());
         doctor.setPassword(doctorDtoPost.getPassword());
         doctor.setAvatar(doctorDtoPost.getAvatar());
-        doctorServiceImpl.update(doctor);
+        doctorService.update(doctor);
         return ResponseEntity.ok(doctorMapper.toDto(doctor));
     }
 
@@ -101,12 +107,9 @@ public class AdminDoctorController {
     })
     @DeleteMapping("{id}")
     public ResponseEntity<Void> deleteDto(@PathVariable("id") long id) {
-        Doctor doctor = doctorServiceImpl.getByKey(id);
-
-
+        Doctor doctor = doctorService.getByKey(id);
         if (doctor != null) {
-
-            doctorServiceImpl.delete(doctor);
+            doctorService.delete(doctor);
             return ResponseEntity.ok().build();
         } else {
             log.info("The current doctor is not found");
