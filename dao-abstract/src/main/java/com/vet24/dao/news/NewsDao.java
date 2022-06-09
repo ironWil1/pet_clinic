0cef0b7de3fd4a3b22bfcd2701912af70a76f7d2
oxmox <martishkaoxanka@gmail.com>
2022-06-09 15:23:25 +1000

    News(Dao+Service)

---
+++ dao-abstract/src/main/java/com/vet24/dao/news/NewsDao.java
@@ -0,0 +1,8 @@
+package com.vet24.dao.news;
+
+import com.vet24.dao.ReadWriteDao;
+import com.vet24.models.news.News;
+
+
+public interface NewsDao extends ReadWriteDao<Long, News> {
+}
---
+++ service-abstract/src/main/java/com/vet24/service/news/NewsService.java
@@ -0,0 +1,7 @@
+package com.vet24.service.news;
+
+import com.vet24.models.news.News;
+import com.vet24.service.ReadWriteService;
+
+public interface NewsService extends ReadWriteService<Long, News> {
+}
