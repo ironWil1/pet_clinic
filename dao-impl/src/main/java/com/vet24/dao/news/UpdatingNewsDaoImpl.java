cd12c66f3e30dbe5084e79c95555cbe699425273
Gena Fedorov <gemor.2010@mail.ru>
2022-07-07 18:15:47 +0300

    dao-impl news removed

--- dao-impl/src/main/java/com/vet24/dao/news/UpdatingNewsDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package com.vet24.dao.news;
-
-import com.vet24.dao.ReadWriteDao;
-import com.vet24.dao.ReadWriteDaoImpl;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class UpdatingNewsDaoImpl extends ReadWriteDaoImpl<Long, UpdatingNews> implements ReadWriteDao<Long, UpdatingNews> {
-}
