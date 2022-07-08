b4af471acb4cb92e515dc5d61a43e39fcd1d1067
justSerjke <serjke3@gmail.com>
2022-07-08 21:39:54 +0400

    Изменения: 1. Скопировал поля из Procedure в ExternalParasiteProcedure 2. Убрал наследование ExternalParasiteProcedure от Procedure 3. Убрал наследование для ExternalParasiteDto 4. Откорректирован ExternalParasiteMapper 5. В TestDataInitializer добавлен метод externalParasiteInitializer()

--- models/src/main/java/com/vet24/models/dto/pet/procedure/ExternalParasiteDto.java
+++ models/src/main/java/com/vet24/models/dto/pet/procedure/ExternalParasiteDto.java
@@ -1,17 +1,53 @@
 package com.vet24.models.dto.pet.procedure;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.vet24.models.dto.OnCreate;
+import com.vet24.models.dto.OnUpdate;
 import com.vet24.models.enums.ProcedureType;
+import com.vet24.models.util.View;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@Data
 @NoArgsConstructor
-public class ExternalParasiteDto extends AbstractNewProcedureDto {
+public class ExternalParasiteDto {
 
-    @JsonCreator
-    public ExternalParasiteDto(LocalDate date, Long medicineId,
+    @JsonView(View.Get.class)
+    Long id;
+
+    @JsonView({View.Put.class, View.Get.class})
+    LocalDate date;
+
+    @JsonView({View.Put.class, View.Get.class})
+    ProcedureType type;
+
+    @NotNull(groups = {OnCreate.class, OnUpdate.class}, message = "Поле medicineId не должно быть null")
+    @JsonView({View.Put.class, View.Get.class})
+    Long medicineId;
+
+    @NotBlank(groups = {OnCreate.class, OnUpdate.class}, message = "Поле medicineBatchNumber не должно быть пустым")
+    @JsonView({View.Put.class, View.Get.class})
+    String medicineBatchNumber;
+
+    @JsonView({View.Put.class, View.Get.class})
+    Boolean isPeriodical;
+
+    @JsonView({View.Put.class, View.Get.class})
+    Integer periodDays;
+
+    public ExternalParasiteDto(Long id, LocalDate date, Long medicineId,
                                String medicineBatchNumber, Boolean isPeriodical, Integer periodDays) {
-        super(date, ProcedureType.EXTERNAL_PARASITE, medicineId, medicineBatchNumber, isPeriodical, periodDays);
+        this.id = id;
+        this.date = date;
+        this.medicineId = medicineId;
+        this.medicineBatchNumber = medicineBatchNumber;
+        this.isPeriodical = isPeriodical;
+        this.periodDays = periodDays;
     }
 }
