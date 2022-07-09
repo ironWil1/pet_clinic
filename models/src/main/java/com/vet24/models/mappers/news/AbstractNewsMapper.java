a90ad6862aa68a047f4e69427567aedccfca5d43
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-09 10:41:10 +0000

    Update AbstractNewsMapper.java

--- models/src/main/java/com/vet24/models/mappers/news/AbstractNewsMapper.java
+++ models/src/main/java/com/vet24/models/mappers/news/AbstractNewsMapper.java
@@ -2,11 +2,25 @@ package com.vet24.models.mappers.news;
 
 
 import com.vet24.models.dto.news.NewsDto;
-import com.vet24.models.enums.NewsType;
 import com.vet24.models.news.News;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
+@Mapper
 public interface AbstractNewsMapper {
-    NewsType getNewsType();
 
-    News abstractNewsDtoToNews(NewsDto newsDto);
+    AbstractNewsMapper INSTANCE = Mappers.getMapper(AbstractNewsMapper.class);
+
+    NewsDto toDto(News news);
+
+    List<NewsDto> toDto(List<News> entities);
+
+    News toEntity(NewsDto dto);
+
+    List<News> toEntity(List<NewsDto> dto);
+
+    void updateEntity(NewsDto dto, @MappingTarget News entity);
 }
