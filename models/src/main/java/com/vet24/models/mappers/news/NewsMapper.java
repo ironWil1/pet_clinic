91d96f83b546c629e151726a6c7dd78ff02d7cd8
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-11 12:35:02 +0000

    Update models/src/main/java/com/vet24/models/mappers/news/NewsMapper.java Deleted models/src/main/java/com/vet24/models/mappers/news/AbstractNewsMapper.java

--- models/src/main/java/com/vet24/models/mappers/news/NewsMapper.java
+++ models/src/main/java/com/vet24/models/mappers/news/NewsMapper.java
@@ -7,35 +7,9 @@ import com.vet24.models.news.News;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
-public class NewsMapper implements DtoMapper<News, NewsDto>, EntityMapper<NewsDto, News> {
-
+public abstract class NewsMapper implements DtoMapper<News, NewsDto>, EntityMapper<NewsDto, News> {
     @Mapping(target = "type", source = "type")
     @Override
-    public NewsDto toDto(News news) {
-        return AbstractNewsMapper.INSTANCE.toDto(news);
-    }
-
-    @Override
-    public List<NewsDto> toDto(List<News> entities) {
-        return AbstractNewsMapper.INSTANCE.toDto(entities);
-    }
-
-    @Override
-    public void updateEntity(NewsDto dto, News entity) {
-        AbstractNewsMapper.INSTANCE.updateEntity(dto, entity);
-    }
-
-    @Override
-    public News toEntity(NewsDto dto) {
-        return AbstractNewsMapper.INSTANCE.toEntity(dto);
-    }
-
-    @Override
-    public List<News> toEntity(List<NewsDto> dto) {
-        return AbstractNewsMapper.INSTANCE.toEntity(dto);
-    }
-
+    public abstract NewsDto toDto(News news);
 }
