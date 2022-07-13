15505101b5c743adcb907d18c87dce051f8c2f1b
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-13 17:43:57 +0000

    Update models/src/main/java/com/vet24/models/news/News.java

--- models/src/main/java/com/vet24/models/news/News.java
+++ models/src/main/java/com/vet24/models/news/News.java
@@ -6,45 +6,69 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @AllArgsConstructor
+@Table(name = "news")
 public class News implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
+    @Column(name = "id")
     private long id;
 
     @Column(name = "type", nullable = false, updatable = false)
     @Enumerated(EnumType.STRING)
     private NewsType type;
 
-    @Column
+    @Column(name = "content")
     private String content;
 
-    @Column
+    @Column(name = "title")
     private String title;
 
-    @Column
+    @Column(name = "is_important")
     private boolean isImportant;
 
-    @Column(columnDefinition = "boolean default false")
+    @Column(name = "published", columnDefinition = "boolean default false")
     private boolean published;
 
-    @Column
+    @Column(name = "end_time")
     private LocalDateTime endTime;
 
     @ElementCollection
-    @Column
+    @Column(name = "pictures")
     List<String> pictures;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        News news = (News) o;
+        return Objects.nonNull(id) && Objects.equals(id, news.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
