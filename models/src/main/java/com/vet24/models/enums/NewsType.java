0a8bc6d0829e7448c8fc2e20f4abed24569be01b
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-13 17:41:37 +0000

    Update models/src/main/java/com/vet24/models/enums/NewsType.java

--- models/src/main/java/com/vet24/models/enums/NewsType.java
+++ models/src/main/java/com/vet24/models/enums/NewsType.java
@@ -1,15 +1,5 @@
 package com.vet24.models.enums;
 
 public enum NewsType {
-        UPDATING("UPDATING"), ADVERTISING_ACTIONS("ADVERTISING_ACTIONS"), DISCOUNTS("DISCOUNTS"), PROMOTION("PROMOTION");
-
-    private String code;
-
-    NewsType(String code) {
-        this.code = code;
-    }
-
-    public String getCode() {
-        return code;
-    }
+        UPDATING, ADVERTISING_ACTIONS, DISCOUNTS, PROMOTION;
 }
