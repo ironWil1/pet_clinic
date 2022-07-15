58cf95cf665a0a0c17329149627f15defa665ba2
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-15 14:24:22 +0000

    Update models/src/main/java/com/vet24/models/mappers/user/ManagerNewsRequestMapper.java

--- models/src/main/java/com/vet24/models/mappers/user/ManagerNewsRequestMapper.java
+++ models/src/main/java/com/vet24/models/mappers/user/ManagerNewsRequestMapper.java
@@ -2,11 +2,12 @@ package com.vet24.models.mappers.user;
 
 import com.vet24.models.dto.user.ManagerNewsRequestDto;
 import com.vet24.models.mappers.DtoMapper;
-import com.vet24.models.mappers.EntityMapper;
 import com.vet24.models.news.News;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
-public interface ManagerNewsRequestMapper extends DtoMapper<News, ManagerNewsRequestDto>, EntityMapper<ManagerNewsRequestDto, News> {
+public interface ManagerNewsRequestMapper extends DtoMapper<News, ManagerNewsRequestDto> {
+    void updateEntity(ManagerNewsRequestDto dto, @MappingTarget News entity);
 }
 
