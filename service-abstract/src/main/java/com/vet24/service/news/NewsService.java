9fcede5950437e3fc8049f354a26692e1b027095
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-16 13:58:58 +0000

    Update service-abstract/src/main/java/com/vet24/service/news/NewsService.java

--- service-abstract/src/main/java/com/vet24/service/news/NewsService.java
+++ service-abstract/src/main/java/com/vet24/service/news/NewsService.java
@@ -1,7 +1,11 @@
 package com.vet24.service.news;
 
+import com.vet24.models.dto.user.ClientNewsResponseDto;
 import com.vet24.models.news.News;
 import com.vet24.service.ReadWriteService;
 
+import java.util.List;
+
 public interface NewsService extends ReadWriteService<Long, News> {
+    List<ClientNewsResponseDto> getClientNewsResponseDto();
 }
