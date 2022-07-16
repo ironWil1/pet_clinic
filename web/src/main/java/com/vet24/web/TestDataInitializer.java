837095a0d28d0370c677fd4aff3485b65981d1bb
KONSTANTIN VASILEV <vasilev110295@mail.ru>
2022-07-16 22:11:01 +0300

    rollback 2

--- web/src/main/java/com/vet24/web/TestDataInitializer.java
+++ web/src/main/java/com/vet24/web/TestDataInitializer.java
@@ -511,6 +511,7 @@ public class TestDataInitializer implements ApplicationRunner {
             appointmentInit();
             notificationAndUserNotificationInit();
             newsInit();
+            profileInit();
         }
     }
 }
