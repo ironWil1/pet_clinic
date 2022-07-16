b92d07de9559add7ecad3bb4852b6857077591bf
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-16 10:31:43 +0000

    Update web/src/main/java/com/vet24/web/controllers/user/ManagerNewsController.java

--- web/src/main/java/com/vet24/web/controllers/user/ManagerNewsController.java
+++ web/src/main/java/com/vet24/web/controllers/user/ManagerNewsController.java
@@ -16,7 +16,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,7 +33,6 @@ import org.webjars.NotFoundException;
 import java.util.List;
 
 
-@Slf4j
 @RestController
 @RequestMapping(value = "api/manager/news")
 @Tag(name = "manager news controller", description = "managerNewsController operations")
