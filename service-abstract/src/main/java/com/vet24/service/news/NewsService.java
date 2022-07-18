c3ea27f15bc580f8d736963909f2f36cbe077ff9
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-18 17:50:28 +0000

    Update service-abstract/src/main/java/com/vet24/service/news/NewsService.java

--- service-abstract/src/main/java/com/vet24/service/news/NewsService.java
+++ service-abstract/src/main/java/com/vet24/service/news/NewsService.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.Map;
 
 public interface NewsService extends ReadWriteService<Long, News> {
-    Map<Long, String> publishNews(List<Long> publishNewsId);
+    Map<Long, String> publishNews(List<Long> ids);
 }
