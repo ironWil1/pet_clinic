43f4fdbaca7dd8bcf8f1d63ba6ef8b9328d1ddb7
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-18 10:23:18 +0000

    Update service-impl/src/main/java/com/vet24/service/news/NewsServiceImpl.java

--- service-impl/src/main/java/com/vet24/service/news/NewsServiceImpl.java
+++ service-impl/src/main/java/com/vet24/service/news/NewsServiceImpl.java
@@ -1,10 +1,19 @@
 package com.vet24.service.news;
 
 import com.vet24.dao.news.NewsDao;
+import com.vet24.models.dto.news.NewsDto;
 import com.vet24.models.news.News;
 import com.vet24.service.ReadWriteServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class NewsServiceImpl extends ReadWriteServiceImpl<Long, News> implements NewsService {
@@ -15,5 +24,35 @@ public class NewsServiceImpl extends ReadWriteServiceImpl<Long, News> implements
     public NewsServiceImpl(NewsDao newsDao) {
         super(newsDao);
         this.newsDao = newsDao;
+    }
+
+    @Transactional
+    @Override
+    public Map<Long, String> unpublishNews(List<Long> unpublishNews) {
+        Map<Long, String> notUnpublishNews = new HashMap<>();
+        List<NewsDto> listNewsDto = newsDao.unpublishNews(unpublishNews);
+
+        List<Long> listNewsId = listNewsDto.stream()
+                .map(NewsDto::getId)
+                .collect(Collectors.toList());
+
+        Set<Long> notExistNewsId = unpublishNews.stream()
+                .filter(i -> !listNewsId.contains(i))
+                .collect(Collectors.toSet());
+
+        List<Long> newsIdEndTime = listNewsDto.stream()
+                .filter(newsDto -> newsDto.getEndTime().isBefore(LocalDateTime.now()))
+                .map(NewsDto::getId)
+                .collect(Collectors.toList());
+
+        for (long id : notExistNewsId) {
+            notUnpublishNews.put(id, "новость не существует");
+        }
+
+        for (long id : newsIdEndTime) {
+            notUnpublishNews.put(id, "endData новости уже прошла");
+        }
+
+        return notUnpublishNews;
     }
 }
