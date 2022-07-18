f7444d19aa5de2cf8b54c96ca8908331edbd93e0
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-18 10:42:17 +0000

    Update web/src/main/java/com/vet24/web/controllers/user/ManagerNewsController.java

--- web/src/main/java/com/vet24/web/controllers/user/ManagerNewsController.java
+++ web/src/main/java/com/vet24/web/controllers/user/ManagerNewsController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.webjars.NotFoundException;
+
 import java.util.List;
 import java.util.Map;
 
