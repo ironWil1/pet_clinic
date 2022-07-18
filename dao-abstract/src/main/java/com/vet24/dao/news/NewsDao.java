e15c2624eb17a0f6bb7c52ee91dc413c8b95baaf
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-18 17:41:24 +0000

    Update dao-abstract/src/main/java/com/vet24/dao/news/NewsDao.java

--- dao-abstract/src/main/java/com/vet24/dao/news/NewsDao.java
+++ dao-abstract/src/main/java/com/vet24/dao/news/NewsDao.java
@@ -1,11 +1,11 @@
 package com.vet24.dao.news;
 
 import com.vet24.dao.ReadWriteDao;
-import com.vet24.models.dto.news.NewsDto;
 import com.vet24.models.news.News;
 
 import java.util.List;
 
 public interface NewsDao extends ReadWriteDao<Long, News> {
-    List<NewsDto> unpublishNews(List<Long> unpublishNews);
+    List<News> getNewsById(List<Long> ids);
+    void unpublishNews(List<Long> ids);
 }
