68b5985c11b7edee6c9047ad43f1c461ad370454
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-18 17:53:15 +0000

    Update dao-impl/src/main/java/com/vet24/dao/news/NewsDaoImpl.java

--- dao-impl/src/main/java/com/vet24/dao/news/NewsDaoImpl.java
+++ dao-impl/src/main/java/com/vet24/dao/news/NewsDaoImpl.java
@@ -1,29 +1,26 @@
 package com.vet24.dao.news;
 
 import com.vet24.dao.ReadWriteDaoImpl;
-import com.vet24.models.dto.news.NewsDto;
 import com.vet24.models.news.News;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class NewsDaoImpl extends ReadWriteDaoImpl<Long, News> implements NewsDao {
     @Override
-    public List<NewsDto> publishNews(List<Long> publishNewsId) {
-        manager.createQuery("update News set published= :published where id in :ids and endTime >= :endTime")
-                .setParameter("published", true)
-                .setParameter("ids", publishNewsId)
-                .setParameter("endTime", LocalDateTime.now())
-                .executeUpdate();
-
-        List<NewsDto> listNewsId = manager.createQuery("select new com.vet24.models.dto.news.NewsDto" +
-                        "(n.id, " +
-                        "n.endTime)" +
-                        " from News n where n.id in :ids", NewsDto.class)
-                .setParameter("ids", publishNewsId)
+    public List<News> getNewsById(List<Long> ids) {
+        List<News> news = manager.createQuery("select n from News n where n.id in :ids")
+                .setParameter("ids", ids)
                 .getResultList();
-        return listNewsId;
+        return news;
+    }
+
+    @Override
+    public void publishNews(List<Long> ids) {
+        manager.createQuery("update News set published= :published where id in :ids")
+                .setParameter("published", true)
+                .setParameter("ids", ids)
+                .executeUpdate();
     }
 }
