f9407e935fe7d74bf5c33bbaedd5ff314ffd87af
justSerjke <serjke3@gmail.com>
2022-07-18 19:16:15 +0400

    Изменения: 1. Запрос в БД UpperCase

--- dao-impl/src/main/java/com/vet24/dao/pet/PetDaoImpl.java
+++ dao-impl/src/main/java/com/vet24/dao/pet/PetDaoImpl.java
@@ -2,22 +2,15 @@ package com.vet24.dao.pet;
 
 import com.vet24.dao.ReadWriteDaoImpl;
 import com.vet24.models.pet.Pet;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
-import org.webjars.NotFoundException;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 
 @Repository
 public class PetDaoImpl extends ReadWriteDaoImpl<Long, Pet> implements PetDao {
     @Override
     public Boolean isPetBelongToClientByPetId(Long petId, Long clientId) {
-        String query = "SELECT CASE WHEN (count(*)>0) then true else false end" +
-                " FROM Pet pet where pet.id =:petId AND pet.client.id =: clientId";
         return manager
-                .createQuery(query, Boolean.class)
+                .createQuery("SELECT CASE WHEN (COUNT (*)>0) THEN TRUE ELSE FALSE END" +
+                        " FROM Pet pet WHERE pet.id =:petId AND pet.client.id =: clientId", Boolean.class)
                 .setParameter("petId", petId)
                 .setParameter("clientId", clientId)
                 .getSingleResult();
