8d5554639438a0149d730297e83364934e352011
Gennady Fedorov <gemor.2010@mail.ru>
2022-07-18 10:10:01 +0000

    Update service-abstract/src/main/java/com/vet24/service/news/NewsService.java

--- service-abstract/src/main/java/com/vet24/service/news/NewsService.java
+++ service-abstract/src/main/java/com/vet24/service/news/NewsService.java
@@ -2,6 +2,9 @@ package com.vet24.service.news;
 
 import com.vet24.models.news.News;
 import com.vet24.service.ReadWriteService;
+import java.util.List;
+import java.util.Map;
 
 public interface NewsService extends ReadWriteService<Long, News> {
+    Map<Long, String> unpublishNews(List<Long> unpublishNews);
 }
