ec5d733271e84243b9167d84fefeff8e8669fa06
greatstep93@gmail.com <djljgfL1250>
2022-07-20 00:50:41 +0400

    не могу победить ошибку после мерджа с мастер веткой
    Description:
    
    Field mapperList in com.vet24.models.mappers.pet.procedure.ProcedureMapper required a bean of type 'java.util.List' that could not be found.

--- dao-impl/src/main/java/com/vet24/dao/pet/PetDaoImpl.java
+++ dao-impl/src/main/java/com/vet24/dao/pet/PetDaoImpl.java
@@ -8,9 +8,8 @@ import org.springframework.stereotype.Repository;
 public class PetDaoImpl extends ReadWriteDaoImpl<Long, Pet> implements PetDao {
     @Override
     public boolean isPetBelongToClient(Long petId, Long clientId) {
-        String query = "SELECT CASE WHEN (count(*)>0) then true else false end FROM Pet WHERE id = :id AND client_id = :client_id";
         return manager
-                .createQuery(query, Boolean.class)
+                .createQuery("SELECT CASE WHEN (count(*)>0) then true else false end FROM Pet WHERE id = :id AND client_id = :client_id", Boolean.class)
                 .setParameter("id", petId)
                 .setParameter("client_id", clientId)
                 .getSingleResult();
