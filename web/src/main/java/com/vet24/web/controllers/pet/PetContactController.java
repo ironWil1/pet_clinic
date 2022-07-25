88ff1329c571229a663608db82c1441c195bfe12
User <ligay_aleksandra@mail.ru>
2022-07-26 01:25:21 +0900

    QrCodeController 1. Изменения 2. Разрешение конфликтов

--- web/src/main/java/com/vet24/web/controllers/pet/PetContactController.java
+++ web/src/main/java/com/vet24/web/controllers/pet/PetContactController.java
@@ -7,6 +7,8 @@ import com.vet24.models.user.Client;
 import com.vet24.service.pet.PetContactService;
 
 import com.vet24.service.pet.PetService;
+import com.vet24.service.user.ClientService;
+import com.vet24.util.qrcode.PetContactQrCodeGenerator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,9 +17,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,12 +42,14 @@ public class PetContactController {
     private final PetContactService petContactService;
     private final PetContactMapper petContactMapper;
     private final PetService petService;
+    private final ClientService clientService;
 
     @Autowired
-    public PetContactController(PetContactService petContactService, PetContactMapper petContactMapper, PetService petService) {
+    public PetContactController(PetContactService petContactService, PetContactMapper petContactMapper, PetService petService, ClientService clientService) {
         this.petContactService = petContactService;
         this.petContactMapper = petContactMapper;
         this.petService = petService;
+        this.clientService = clientService;
     }
 
     @Operation(summary = "Получние Контакта питомца по его ID")
@@ -75,6 +82,30 @@ public class PetContactController {
             petContactMapper.updateEntity(petContactDto, petContact);
             petContactService.update(petContact);
             return new ResponseEntity<>(petContactDto, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @Value("${application.domain.name}")
+    private String URL;
+
+    @Operation(summary = "Создание QR кода для Контакта питомца")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "QR код был успешно создан"),
+            @ApiResponse(responseCode = "400", description = "Питомец не найден или Вам не принадлежит")
+    })
+    @GetMapping(value = "/qr?{petId}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> createPetContactQrCode(@PathVariable("petId") Long id) {
+        Client client = clientService.getCurrentClientWithPets();
+        if (petService.isExistByPetIdAndClientId(id, client.getId())) {
+            PetContact petContact = petContactService.getByKey(id);
+            String urlToAlertPetContact = URL + "/api/petFound?code=" + petContact.getCode();
+            String sb = "Имя питомца - " + petContact.getPet().getName() + ", " +
+                    "Владелец - " + petContact.getOwnerName() + ", " +
+                    "Адрес - " + petContact.getAddress() + ", " +
+                    "Телефон - " + petContact.getPhone() + ", " +
+                    "Чтобы сообщить владельцу о находке перейдите по адресу - " + urlToAlertPetContact;
+            return ResponseEntity.ok(PetContactQrCodeGenerator.generatePetContactQrCodeImage(sb));
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
