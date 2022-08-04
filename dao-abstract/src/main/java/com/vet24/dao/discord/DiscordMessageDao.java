d8afe774d1dc3e85ec1e8098b8c261cf59fa4af8
Gennady Fedorov <gemor.2010@mail.ru>
2022-08-04 13:42:33 +0000

    #302 model discord message

---
+++ dao-abstract/src/main/java/com/vet24/dao/discord/DiscordMessageDao.java
@@ -0,0 +1,9 @@
+package com.vet24.dao.discord;
+
+
+import com.vet24.dao.ReadWriteDao;
+import com.vet24.models.discord.DiscordMessage;
+
+public interface DiscordMessageDao extends ReadWriteDao<Long, DiscordMessage> {
+
+}
