8c82de61404200cf3bef4d96194f592a0a122ebb
Max Okorokov <okoro99@mail.ru>
2022-08-17 14:04:01 +0000

    Resolve "CONTROLLER - getAllReproductions"

--- service-impl/src/main/java/com/vet24/service/pet/reproduction/ReproductionServiceImpl.java
+++ service-impl/src/main/java/com/vet24/service/pet/reproduction/ReproductionServiceImpl.java
@@ -6,6 +6,8 @@ import com.vet24.service.ReadWriteServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReproductionServiceImpl extends ReadWriteServiceImpl<Long, Reproduction> implements ReproductionService {
 
@@ -15,5 +17,10 @@ public class ReproductionServiceImpl extends ReadWriteServiceImpl<Long, Reproduc
     public ReproductionServiceImpl(ReproductionDao reproductionDao) {
         super(reproductionDao);
         this.reproductionDao = reproductionDao;
+    }
+
+    @Override
+    public List<Reproduction> getAllByPetId(Long petId) {
+        return reproductionDao.getAllByPetId(petId);
     }
 }
