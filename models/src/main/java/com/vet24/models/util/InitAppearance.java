417baf740a6488bb89baa970b09849919b963ed6
HP <okoro99@mail.ru>
2022-08-24 14:50:04 +0300

    выпадает ошибка org.hibernate.exception.SQLGrammarException: could not execute statement

--- models/src/main/java/com/vet24/models/util/InitAppearance.java
+++ models/src/main/java/com/vet24/models/util/InitAppearance.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
@@ -18,13 +19,27 @@ public class InitAppearance {
     @PersistenceContext
     protected EntityManager manager;
 
-
+    @Autowired
+    EntityManagerFactory entityManagerFactory;
+    EntityTransaction txn = null;
+    String sql = "CREATE TABLE IF NOT EXISTS color (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
+            "color VARCHAR(255))";
 
     @PostConstruct
     public void colorTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS color (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
-                "color VARCHAR(255))";
-        manager.createNativeQuery(sql).executeUpdate();
+        try {
+            manager = entityManagerFactory.createEntityManager();
+            txn = manager.getTransaction();
+            txn.begin();
+            manager.createNativeQuery(sql).executeUpdate();
+            txn.commit();
+        } catch ( Throwable e ) {
+            if ( txn != null && txn.isActive() ) {
+                txn.rollback();
+            }
+            throw e;
+        }
+
     }
 
     @PostConstruct
