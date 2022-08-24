3e752e8112a5da5a5aa0ec7c8ba669ebfb12c585
Rognir <www.cwnik111@gmail.com>
2022-08-24 22:57:50 +0300

    creating endpoint PetFoundClientController

--- dao-abstract/src/main/java/com/vet24/dao/pet/PetFoundDao.java
+++ dao-abstract/src/main/java/com/vet24/dao/pet/PetFoundDao.java
@@ -1,8 +1,18 @@
 package com.vet24.dao.pet;
 
 import com.vet24.dao.ReadWriteDao;
+import com.vet24.models.pet.Pet;
 import com.vet24.models.pet.PetFound;
+import com.vet24.models.user.User;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PetFoundDao extends ReadWriteDao<Long, PetFound> {
 
+    Optional<User> findByLogin(String login);
+
+    List<Pet> getClientPet(Long petId, Long userId);
+
+    List<PetFound> getPetFoundById(Long petId);
 }
--- service-impl/src/main/java/com/vet24/service/pet/PetFoundServiceImpl.java
+++ service-impl/src/main/java/com/vet24/service/pet/PetFoundServiceImpl.java
@@ -1,9 +1,15 @@
 package com.vet24.service.pet;
 
 import com.vet24.dao.pet.PetFoundDao;
+import com.vet24.models.pet.Pet;
 import com.vet24.models.pet.PetFound;
+import com.vet24.models.user.User;
 import com.vet24.service.ReadWriteServiceImpl;
+
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PetFoundServiceImpl extends ReadWriteServiceImpl<Long, PetFound> implements PetFoundService {
@@ -13,5 +19,20 @@ public class PetFoundServiceImpl extends ReadWriteServiceImpl<Long, PetFound> im
     public PetFoundServiceImpl(PetFoundDao petFoundDao) {
         super(petFoundDao);
         this.petFoundDao = petFoundDao;
+    }
+
+    @Override
+    public Optional<User> findByLogin(String login) {
+        return petFoundDao.findByLogin(login);
+    }
+
+    @Override
+    public List<Pet> getClientPet(Long petId, Long userId) {
+        return petFoundDao.getClientPet(petId, userId);
+    }
+
+    @Override
+    public List<PetFound> getPetFoundById(Long petId) {
+        return petFoundDao.getPetFoundById(petId);
     }
 }
