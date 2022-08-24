2460212c160271789564a3a7e034de4fc2925c9b
HP <okoro99@mail.ru>
2022-08-24 14:50:04 +0300

    выпадает ошибка org.hibernate.exception.SQLGrammarException: could not execute statement

--- models/src/main/java/com/vet24/models/util/InitAppearance.java
+++ models/src/main/java/com/vet24/models/util/InitAppearance.java
@@ -9,52 +9,12 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-<<<<<<< HEAD
-import javax.persistence.EntityTransaction;
-=======
->>>>>>> 835b6bca (если executeUpdate оставляет,то выпадает ошибка TransactionRequiredException Executing an update/delete query, если убираю,то табл просто не сохраняется)
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 @Component
 public class InitAppearance {
 
-<<<<<<< HEAD
-    //    @PersistenceContext
-    @Autowired
-    private EntityManager manager;
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
-    private EntityTransaction txn = null;
-    //    String sql = "CREATE TABLE color (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
-//            "color VARCHAR(255))";
-    String sql2 = "CREATE TABLE if not exists pet_color (id serial  PRIMARY KEY," +
-            "            color TEXT not null unique )";
-    String sql3 = "CREATE TABLE if not exists  pet_breed (id serial  PRIMARY KEY," +
-            "pet_type TEXT not null, breed TEXT not null, UNIQUE (pet_type,breed));";
-
-    // без EntityManagerFactory и EntityTransaction я не смог создать таблицы
-
-    @Transactional
-    @PostConstruct
-    public void colorTable() {
-        try {
-            manager = entityManagerFactory.createEntityManager();
-            txn = manager.getTransaction();
-            txn.begin();
-            manager.createNativeQuery(sql2).executeUpdate();
-            manager.createNativeQuery(sql3).executeUpdate();
-            txn.commit();
-        } catch (Throwable e) {
-            if (txn != null && txn.isActive()) {
-                txn.rollback();
-            }
-            throw e;
-        }
-    }
-
-
-=======
     @PersistenceContext
     protected EntityManager manager;
 
@@ -71,5 +31,4 @@ public class InitAppearance {
     public void BreedTable() {
 
     }
->>>>>>> 835b6bca (если executeUpdate оставляет,то выпадает ошибка TransactionRequiredException Executing an update/delete query, если убираю,то табл просто не сохраняется)
 }
