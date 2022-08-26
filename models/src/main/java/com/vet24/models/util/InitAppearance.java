3b8f50601d93c21cd3182e421064d6719dc19ee6
HP <okoro99@mail.ru>
2022-08-26 13:07:37 +0300

    gg

--- models/src/main/java/com/vet24/models/util/InitAppearance.java
+++ models/src/main/java/com/vet24/models/util/InitAppearance.java
@@ -9,26 +9,45 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 @Component
 public class InitAppearance {
 
-    @PersistenceContext
-    protected EntityManager manager;
+    //    @PersistenceContext
+    @Autowired
+    private EntityManager manager;
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+    private EntityTransaction txn = null;
+    //    String sql = "CREATE TABLE color (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
+//            "color VARCHAR(255))";
+    String sql2 = "CREATE TABLE if not exists pet_color (id serial  PRIMARY KEY," +
+            "            color TEXT not null unique )";
+    String sql3 = "CREATE TABLE if not exists  pet_breed (id serial  PRIMARY KEY," +
+            "pet_type TEXT not null, breed TEXT not null, UNIQUE (pet_type,breed));";
 
+    // без EntityManagerFactory и EntityTransaction я не смог создать таблицы
 
-
+    @Transactional
     @PostConstruct
     public void colorTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS color (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
-                "color VARCHAR(255))";
-        manager.createNativeQuery(sql).executeUpdate();
+        try {
+            manager = entityManagerFactory.createEntityManager();
+            txn = manager.getTransaction();
+            txn.begin();
+            manager.createNativeQuery(sql2).executeUpdate();
+            manager.createNativeQuery(sql3).executeUpdate();
+            txn.commit();
+        } catch (Throwable e) {
+            if (txn != null && txn.isActive()) {
+                txn.rollback();
+            }
+            throw e;
+        }
     }
 
-    @PostConstruct
-    public void BreedTable() {
 
-    }
 }
