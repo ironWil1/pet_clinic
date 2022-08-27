10d86e177775fe7733b748c3d2d04bee68e669db
kotamadeo <kotamadeo@gmail.com>
2022-08-28 00:42:21 +0300

    commit for pipelines.

--- web/src/test/java/com/vet24/web/controllers/found/PetFoundControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/found/PetFoundControllerTest.java
@@ -19,7 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 public class PetFoundControllerTest extends ControllerAbstractIntegrationTest {
-
     private final String URL = "/api/petFound";
     private String token;
 
