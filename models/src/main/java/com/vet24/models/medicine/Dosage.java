3e8b69429317fbbff0e16705eb42601cc0f6a939
SValya <Samohva19@yandex.ru>
2022-10-19 18:04:44 +0300

    поправлены импорты

--- models/src/main/java/com/vet24/models/medicine/Dosage.java
+++ models/src/main/java/com/vet24/models/medicine/Dosage.java
@@ -1,9 +1,21 @@
 package com.vet24.models.medicine;
 
 import com.vet24.models.enums.DosageType;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
 
 @Entity
 @Getter
--- models/src/main/java/com/vet24/models/medicine/Medicine.java
+++ models/src/main/java/com/vet24/models/medicine/Medicine.java
@@ -8,7 +8,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
