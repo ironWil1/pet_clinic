c41098bde79308637920ef312c24382fcfa1cc6e
Gladkikh Olga <gladkikh.o21@gmail.com>
2022-10-24 22:42:34 +0300

    Refactor test names in CheckExistControllerTest and transfer CheckExistController to test package

--- web/src/test/java/com/vet24/web/controllers/annotations/CheckExistControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/annotations/CheckExistControllerTest.java
@@ -33,7 +33,7 @@ public class CheckExistControllerTest extends ControllerAbstractIntegrationTest 
             "/datasets/controllers/checkExistControllerTest/user-entities.yml",
             "/datasets/controllers/checkExistControllerTest/pet-entities.yml",
             "/datasets/controllers/checkExistControllerTest/clinical-examination.yml"})
-    public void testGetExaminationByPetIdSuccessBecauseIdExist() throws Exception {
+    public void testCheckEntityExistOneParameter() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(URI + "/exam")
                         .param("petId", "100")
                         .header("Authorization", "Bearer " + token))
@@ -45,7 +45,7 @@ public class CheckExistControllerTest extends ControllerAbstractIntegrationTest 
             "/datasets/controllers/checkExistControllerTest/user-entities.yml",
             "/datasets/controllers/checkExistControllerTest/pet-entities.yml",
             "/datasets/controllers/checkExistControllerTest/clinical-examination.yml"})
-    public void testGetExaminationByPetIdAndIdNotExist() throws Exception {
+    public void testCheckEntityNotExistOneParameter() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(URI + "/exam")
                         .param("petId", "77")
                         .header("Authorization", "Bearer " + token))
@@ -60,7 +60,7 @@ public class CheckExistControllerTest extends ControllerAbstractIntegrationTest 
             "/datasets/controllers/checkExistControllerTest/user-entities.yml",
             "/datasets/controllers/checkExistControllerTest/pet-entities.yml",
             "/datasets/controllers/checkExistControllerTest/clinical-examination.yml"})
-    public void testGetExaminationByDataAndPetIdSuccessBecauseIdExist() throws Exception {
+    public void testCheckEntityExistTwoParameters() throws Exception {
         String data = "2020-01-01";
         mockMvc.perform(MockMvcRequestBuilders.get(URI + "/exams")
                         .param(data, "2020-01-01")
@@ -75,7 +75,7 @@ public class CheckExistControllerTest extends ControllerAbstractIntegrationTest 
             "/datasets/controllers/checkExistControllerTest/user-entities.yml",
             "/datasets/controllers/checkExistControllerTest/pet-entities.yml",
             "/datasets/controllers/checkExistControllerTest/clinical-examination.yml"})
-    public void testGetExaminationByDataAndPetIdAndIdNotExist() throws Exception {
+    public void testCheckEntityNotExistTwoParameters() throws Exception {
         String data = "2022-01-01";
         mockMvc.perform(MockMvcRequestBuilders.get(URI + "/exam")
                         .param(data, "2022-01-01")
@@ -91,7 +91,7 @@ public class CheckExistControllerTest extends ControllerAbstractIntegrationTest 
             "/datasets/controllers/checkExistControllerTest/user-entities.yml",
             "/datasets/controllers/checkExistControllerTest/pet-entities.yml",
             "/datasets/controllers/checkExistControllerTest/clinical-examination.yml"})
-    public void testGetExaminationByTwoIdsSuccessBecauseIdsExist() throws Exception {
+    public void testCheckEntityExistTwoIdParameters() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(URI + "/103/exam")
                         .param("petId", "104")
                         .header("Authorization", "Bearer " + token))
@@ -104,7 +104,7 @@ public class CheckExistControllerTest extends ControllerAbstractIntegrationTest 
             "/datasets/controllers/checkExistControllerTest/user-entities.yml",
             "/datasets/controllers/checkExistControllerTest/pet-entities.yml",
             "/datasets/controllers/checkExistControllerTest/clinical-examination.yml"})
-    public void testGetExaminationByTwoIdsAndIdsNotExist() throws Exception {
+    public void testCheckEntityNotExistTwoIdParameters() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(URI + "/103/exam")
                         .param("petId", "88")
                         .header("Authorization", "Bearer " + token))
