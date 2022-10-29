a1232364842e0e5b1ea0934c5ea8ecb3493006c5
IronWil1 <aidarziyangirov@gmail.com>
2022-10-29 23:17:05 +0300

    Устранен баг по назначению роли доктора при его создании в контроллере AdminDoctorController

--- models/src/main/java/com/vet24/models/mappers/user/DoctorPostMapper.java
+++ models/src/main/java/com/vet24/models/mappers/user/DoctorPostMapper.java
@@ -2,8 +2,10 @@ package com.vet24.models.mappers.user;
 
 import com.vet24.models.dto.user.DoctorDtoPost;
 import com.vet24.models.dto.user.UserDoctorDto;
+import com.vet24.models.enums.RoleNameEnum;
 import com.vet24.models.mappers.DtoMapper;
 import com.vet24.models.mappers.EntityMapper;
+import com.vet24.models.user.Role;
 import com.vet24.models.user.User;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
@@ -11,4 +13,19 @@ import org.springframework.stereotype.Component;
 @Component
 @Mapper(componentModel = "spring")
 public interface DoctorPostMapper extends DtoMapper<User, DoctorDtoPost>, EntityMapper<UserDoctorDto, User> {
+    @Override
+    default User toEntity(DoctorDtoPost dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setId(dto.getId());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setRole(new Role(RoleNameEnum.DOCTOR));
+
+        return user;
+    }
 }
