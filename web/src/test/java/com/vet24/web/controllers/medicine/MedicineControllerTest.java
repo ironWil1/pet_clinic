0723c6e96be3847e527a11e5e77f61dc9a876779
SValya <Samohva19@yandex.ru>
2022-11-03 12:31:10 +0300

    тесты дозировки перенесены в MedicineControllerTest

--- web/src/test/java/com/vet24/web/controllers/medicine/MedicineControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/medicine/MedicineControllerTest.java
@@ -1,11 +1,14 @@
 package com.vet24.web.controllers.medicine;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.vet24.models.dto.medicine.DosageRequestDto;
 import com.vet24.models.dto.medicine.MedicineRequestDto;
+import com.vet24.models.enums.DosageType;
 import com.vet24.models.medicine.Medicine;
 import com.vet24.web.ControllerAbstractIntegrationTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.hamcrest.core.Is;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -29,6 +32,8 @@ public class MedicineControllerTest extends ControllerAbstractIntegrationTest {
     MedicineRequestDto medicineRequestEmptyThirdField;
     MedicineRequestDto medicineRequestEmptyFourthField;
     MedicineRequestDto medicineRequestDto1;
+    DosageRequestDto dosageRequestDto;
+    DosageRequestDto dosageRequestDtoExists;
 
     @Before
     public void createNewMedicineAndMedicineDto() {
@@ -41,12 +46,23 @@ public class MedicineControllerTest extends ControllerAbstractIntegrationTest {
     }
 
     @Before
+    public void createNewDosageAndDosageDto() {
+        this.dosageRequestDto = new DosageRequestDto(111, DosageType.DROPS);
+        this.dosageRequestDtoExists = new DosageRequestDto(102, DosageType.PILLS );
+
+    }
+
+    @Before
     public void setToken() throws Exception {
         token = getAccessToken("manager1@email.com","manager");
     }
 
     private long getCount() {
         return entityManager.createQuery("SELECT COUNT(m) FROM Medicine m", Long.class).getSingleResult();
+    }
+
+    private long getCountDosages(Long medicineId) {
+        return entityManager.createQuery("SELECT COUNT(d) FROM Dosage d WHERE d.medicine.id = :medicineId", Long.class).setParameter("medicineId",medicineId).getSingleResult();
     }
 
     @Test
@@ -249,5 +265,78 @@ public class MedicineControllerTest extends ControllerAbstractIntegrationTest {
                         .param("searchText","")
                         .header("Authorization", "Bearer " + token))
                 .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    //Работа с дозировкой
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/controllers/medicineController/user-entities.yml",
+            "/datasets/controllers/medicineController/medicine.yml",
+            "/datasets/controllers/medicineController/dosage.yml"})
+    public void getAllDosagesSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(URI + "/{medicineId}/dosage", 101L)
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$[0].id", Is.is(101)))
+                .andExpect(jsonPath("$[0].dosageSize", Is.is(101)))
+                .andExpect(jsonPath("$[0].dosageType", Is.is("PILLS")))
+
+                .andExpect(jsonPath("$[1].id", Is.is(102)))
+                .andExpect(jsonPath("$[1].dosageSize", Is.is(102)))
+                .andExpect(jsonPath("$[1].dosageType", Is.is("PILLS")));
+        assertThat(2).isEqualTo(getCountDosages(101L));
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/controllers/medicineController/user-entities.yml",
+            "/datasets/controllers/medicineController/medicine.yml",
+            "/datasets/controllers/medicineController/dosage.yml"})
+    public void createDosageSuccess() throws Exception {
+        long beforeCount = getCountDosages(100L);
+        mockMvc.perform(MockMvcRequestBuilders.post(URI + "/{medicineId}/dose", 100L)
+                        .header("Authorization", "Bearer " + token)
+                        .content(objectMapper.valueToTree(dosageRequestDto).toString())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+        assertThat(++beforeCount).isEqualTo(getCountDosages(100L));
+    }
+
+    //400 - Данная Дозировка уже существует
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/controllers/medicineController/user-entities.yml",
+            "/datasets/controllers/medicineController/medicine.yml",
+            "/datasets/controllers/medicineController/dosage.yml"})
+    public void createExistDosage() throws Exception {
+        long beforeCount = getCountDosages(101L);
+        mockMvc.perform(MockMvcRequestBuilders.post(URI + "/{medicineId}/dose", 101L)
+                        .header("Authorization", "Bearer " + token)
+                        .content(objectMapper.valueToTree(dosageRequestDtoExists).toString())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        assertThat(beforeCount).isEqualTo(getCountDosages(101L));
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/controllers/medicineController/user-entities.yml",
+            "/datasets/controllers/medicineController/medicine.yml",
+            "/datasets/controllers/medicineController/dosage.yml"})
+    public void deleteDosageSuccess() throws Exception {
+        long beforeCount = getCountDosages(101L);
+        mockMvc.perform(MockMvcRequestBuilders.delete(URI + "/{medicineId}/dosage/{dosageId}", 101L, 102L)
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        assertThat(--beforeCount).isEqualTo(getCountDosages(101L));
+    }
+
+    //400 - Дозировка другого лекарства
+    @Test
+    @DataSet(cleanBefore = true, value = {"/datasets/controllers/medicineController/user-entities.yml",
+            "/datasets/controllers/medicineController/medicine.yml",
+            "/datasets/controllers/medicineController/dosage.yml"})
+    public void deleteNotExistDosage() throws Exception {
+        long beforeCount = getCountDosages(100L);
+        mockMvc.perform(MockMvcRequestBuilders.delete(URI + "/{medicineId}/dosage/{dosageId}", 100L, 102L)
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        assertThat(beforeCount).isEqualTo(getCountDosages(100L));
     }
 }
