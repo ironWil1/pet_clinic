c3c4fd74632278e45e018996b130951e0d8ccada
IronWil1 <aidarziyangirov@gmail.com>
2022-11-05 23:08:46 +0300

    Integration Tests For AppearanceManagerController - Color

--- web/src/test/java/com/vet24/web/controllers/user/AppearanceManagerControllerTest.java
+++ web/src/test/java/com/vet24/web/controllers/user/AppearanceManagerControllerTest.java
@@ -2,6 +2,7 @@ package com.vet24.web.controllers.user;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.vet24.service.pet.appearance.BreedService;
+import com.vet24.service.pet.appearance.ColorService;
 import com.vet24.web.ControllerAbstractIntegrationTest;
 import org.hamcrest.core.Is;
 import org.junit.Before;
@@ -14,17 +15,21 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 public class AppearanceManagerControllerTest extends ControllerAbstractIntegrationTest {
-
 
     private final String URI = "/api/manager/appearance";
 
     @Autowired
     private BreedService breedService;
+
+    @Autowired
+    private ColorService colorService;
 
     private String token;
 
@@ -181,4 +186,92 @@ public class AppearanceManagerControllerTest extends ControllerAbstractIntegrati
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
         assertThat(sizeBefore).isEqualTo(breedService.getAllBreeds().size());
     }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {
+            "datasets/controllers/user/appearanceManagerController/user-entities.yml",
+            "datasets/controllers/user/appearanceManagerController/pet-color.yml"})
+    public void getColorSuccessTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(URI + "/color")
+                        //test trim of incorrect param insert
+                        .param("color", "   black")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$[0]", Is.is("black")));
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {
+            "datasets/controllers/user/appearanceManagerController/user-entities.yml",
+            "datasets/controllers/user/appearanceManagerController/pet-color.yml"})
+    public void getColorEmptyParamTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(URI + "/color")
+                        .param("color", "")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(colorService.getAllColors())))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {
+            "datasets/controllers/user/appearanceManagerController/user-entities.yml",
+            "datasets/controllers/user/appearanceManagerController/pet-color.yml"})
+    public void addColorSuccessTest() throws Exception {
+        int sizeBeforeTest = colorService.getAllColors().size();
+        mockMvc.perform(MockMvcRequestBuilders.post(URI + "/color")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper()
+                        .writeValueAsString(List.of("green"))))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        assertThat(colorService.getAllColors().size()).isEqualTo(sizeBeforeTest + 1);
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {
+            "datasets/controllers/user/appearanceManagerController/user-entities.yml",
+            "datasets/controllers/user/appearanceManagerController/pet-color.yml"})
+    public void addColorIfPresentTest() throws Exception {
+        int sizeBefore = colorService.getAllColors().size();
+        mockMvc.perform(MockMvcRequestBuilders.post(URI + "/color")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper()
+                                .writeValueAsString(List.of("black"))))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        assertThat(sizeBefore).isEqualTo(colorService.getAllColors().size());
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {
+            "datasets/controllers/user/appearanceManagerController/user-entities.yml",
+            "datasets/controllers/user/appearanceManagerController/pet-color.yml"})
+    public void deleteColorSuccessTest() throws Exception {
+        int sizeBefore = colorService.getAllColors().size();
+        mockMvc.perform(MockMvcRequestBuilders.delete(URI + "/color")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper()
+                                .writeValueAsString(List.of("black"))))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        assertThat(colorService.getAllColors().size()).isEqualTo(sizeBefore - 1);
+    }
+
+    @Test
+    @DataSet(cleanBefore = true, value = {
+            "datasets/controllers/user/appearanceManagerController/user-entities.yml",
+            "datasets/controllers/user/appearanceManagerController/pet-color.yml"})
+    public void deleteColorIfNotPresentTest() throws Exception {
+        int sizeBefore = colorService.getAllColors().size();
+        mockMvc.perform(MockMvcRequestBuilders.delete(URI + "/color")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper()
+                                .writeValueAsString(List.of("grey"))))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        assertThat(sizeBefore).isEqualTo(colorService.getAllColors().size());
+    }
+
+
 }
